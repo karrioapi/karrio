8dffc8d1f014face74019d3b0f561871d6b437d9
Dan Kobina <danielkobina@gmail.com>
2018-04-04 15:12:14 -0400

    introduce setup.py to make package installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py_soap',
+      version='0.1.dev0',
+      description='SOAP Python Data Structure',
+      url='https://github.com/OpenShip/py_soap',
+      author='DanH91',
+      author_email='danielk.developer@gmail.com',
+      license='MIT',
+      packages=['soap'],
+      zip_safe=False)
