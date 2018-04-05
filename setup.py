cafb280cfe9cecbb647f7fccd0278e667ebfbc3b
Dan Kobina <danielkobina@gmail.com>
2018-04-05 01:10:19 -0400

    add setup.py to make the project installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='open_mappers',
+      version='0.1.dev0',
+      description='Shipping providers gateways and mappers',
+      url='https://github.com/OpenShip/open_mappers',
+      author='DanH91',
+      author_email='danielk.developer@gmail.com',
+      license='MIT',
+      packages=['mappers', 'domain'],
+      zip_safe=False)
