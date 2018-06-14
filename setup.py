07d9caa28b23b7d750cb7cd16c5cf5c99d89eb4c
Dan Kobina <danielkobina@gmail.com>
2018-06-13 20:12:48 -0400

    update setup to reflect repo renaming

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='py_soap',
+setup(name='py-soap',
       version='0.1.dev0',
       description='SOAP Python Data Structure',
-      url='https://github.com/OpenShip/py_soap',
+      url='https://github.com/OpenShip/py-soap',
       author='DanH91',
       author_email='danielk.developer@gmail.com',
       license='MIT',
