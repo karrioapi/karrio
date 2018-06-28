35e413b6a9b14dfbed8398073afb26a1354abed6
Dan Kobina <danielkobina@gmail.com>
2018-06-27 22:57:35 -0400

    implement get tracking for dhl proxy

--- openship/mappers/dhl/dhl_proxy.py
+++ openship/mappers/dhl/dhl_proxy.py
@@ -1,6 +1,6 @@
 from io import StringIO
 from gds_helpers import export, to_xml, request as http
-from pydhl import DCT_Response_global as Response, DCT_req_global as Request
+from pydhl import DCT_Response_global as Response, DCT_req_global as Request, tracking_request_known as Track
 from .dhl_mapper import DHLMapper, DHLClient
 from ...domain.proxy import Proxy
 
@@ -11,7 +11,21 @@ class DHLProxy(Proxy):
         self.mapper = DHLMapper(client) if mapper is None else mapper
 
     def get_quotes(self, DCTRequest_: Request.DCTRequest):
-        xmlElt = export(DCTRequest_, name_='p:DCTRequest', namespacedef_='schemaVersion="1.0" xmlns:p="http://www.dhl.com" xmlns:p1="http://www.dhl.com/datatypes" xmlns:p2="http://www.dhl.com/DCTRequestdatatypes" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com DCT-req.xsd "')
+        xmlElt = export(
+            DCTRequest_, 
+            name_='p:DCTRequest', 
+            namespacedef_='schemaVersion="1.0" xmlns:p="http://www.dhl.com" xmlns:p1="http://www.dhl.com/datatypes" xmlns:p2="http://www.dhl.com/DCTRequestdatatypes" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com DCT-req.xsd "'
+        )
+
+        result = http(url=self.client.server_url, data=bytearray(xmlElt, "utf-8"), headers={'Content-Type': 'application/xml'}, method="POST")
+        return to_xml(result)
+
+    def get_trackings(self, KnownTrackingRequest_: Track.KnownTrackingRequest):
+        xmlElt = export(
+            KnownTrackingRequest_, 
+            name_='req:KnownTrackingRequest',
+            namespacedef_='xmlns:req="http://www.dhl.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com TrackingRequestKnown.xsd"'
+        )
 
         result = http(url=self.client.server_url, data=bytearray(xmlElt, "utf-8"), headers={'Content-Type': 'application/xml'}, method="POST")
         return to_xml(result)
