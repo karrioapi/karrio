7ce68a7ac25fad74412d48942f8eb574c4cf3d44
Dan Kobina <danielkobina@gmail.com>
2018-07-07 08:57:18 -0400

    add missing arguments 'payment_country_code'

--- openship/domain/entities/definitions.py
+++ openship/domain/entities/definitions.py
@@ -4,13 +4,14 @@ from collections import namedtuple
 
 ''' shipment details Type definition '''
 class shipment_details_type(namedtuple("shipment_details_type", "packages insurance charges_payment number_of_packages packaging_type is_dutiable currency total_weight weight_unit dimension_unit")):
-    def __new__(cls, packages, insurance=None, charges_payment=None, number_of_packages=None, packaging_type=None, is_dutiable="N", currency=None, total_weight=None, weight_unit="LB", dimension_unit="IN"):
+    def __new__(cls, packages, insurance=None, charges_payment=None, number_of_packages=None, packaging_type=None, is_dutiable="N", currency=None, total_weight=None, weight_unit="LB", dimension_unit="IN", payment_country_code=None):
         return super(cls, shipment_details_type).__new__(
             cls,
             list(map(lambda p: package_type(**p), packages)),
             insurance if insurance is None else insurance_type(**insurance),
             charges_payment if charges_payment is None else charges_payment_type(**charges_payment),
-            number_of_packages, packaging_type, is_dutiable, currency, total_weight, weight_unit, dimension_unit
+            number_of_packages, packaging_type, is_dutiable, currency, total_weight, 
+            weight_unit, dimension_unit, payment_country_code
         )
 
 
