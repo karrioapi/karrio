d4c366062e4aad3e145dfc2790da0f052ec01c12
Dan Kobina <danielkobina@gmail.com>
2018-07-07 09:01:53 -0400

    add missing positional argument declaration

--- openship/domain/entities/definitions.py
+++ openship/domain/entities/definitions.py
@@ -3,7 +3,7 @@ from ...domain import entities as E
 from collections import namedtuple
 
 ''' shipment details Type definition '''
-class shipment_details_type(namedtuple("shipment_details_type", "packages insurance charges_payment number_of_packages packaging_type is_dutiable currency total_weight weight_unit dimension_unit")):
+class shipment_details_type(namedtuple("shipment_details_type", "packages insurance charges_payment number_of_packages packaging_type is_dutiable currency total_weight weight_unit dimension_unit payment_country_code")):
     def __new__(cls, packages, insurance=None, charges_payment=None, number_of_packages=None, packaging_type=None, is_dutiable="N", currency=None, total_weight=None, weight_unit="LB", dimension_unit="IN", payment_country_code=None):
         return super(cls, shipment_details_type).__new__(
             cls,
