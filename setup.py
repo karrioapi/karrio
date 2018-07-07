be203789a248a0cf700ef4b5c08cdc6cceac5781
Dan Kobina <danielkobina@gmail.com>
2018-07-07 10:03:15 -0400

    update version to 1.0-beta.2 with py-dhl and py-fedex upgraded to 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='openship',
-      version='1.0-beta.1',
+      version='1.0-beta.2',
       description='Shipping carriers mappers and proxies',
       url='https://github.com/OpenShip/openship',
       author='DanH91',
@@ -9,14 +9,14 @@ setup(name='openship',
       license='LGPL',
       packages=['openship'],
       install_requires=[
-            'py-fedex==1.0',
-            'py-dhl==1.0',
+            'py-fedex==1.1',
+            'py-dhl==1.1',
             'py-soap==1.0',
             'gds-helpers==0.3.0',
       ],
       dependency_links=[
-            'git+https://github.com/OpenShip/py-fedex.git@v1.0#egg=py-fedex-1.0',
-            'git+https://github.com/OpenShip/py-dhl.git@v1.0#egg=py-dhl-1.0',
+            'git+https://github.com/OpenShip/py-fedex.git@v1.1#egg=py-fedex-1.1',
+            'git+https://github.com/OpenShip/py-dhl.git@v1.1#egg=py-dhl-1.1',
             'git+https://github.com/OpenShip/py-soap.git@v1.0#egg=py-soap-1.0',
             'git+https://github.com/OpenShip/generateDs-helpers.git@v0.3.0#egg=gds-helpers-0.3.0',
       ],
