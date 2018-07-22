920515b32cd2c532e7c0b59c673088843c17f1d5
Dan Kobina <danielkobina@gmail.com>
2018-07-22 19:47:47 -0400

    update ups_proxy to use pysoap create_envelope and clean_namespaces

--- openship/mappers/ups/ups_proxy.py
+++ openship/mappers/ups/ups_proxy.py
@@ -1,6 +1,7 @@
 from gds_helpers import to_xml, export, request as http
 from pyups import freight_rate as Rate, package_track as Track
 from pysoap.envelope import Body, Envelope, Header
+from pysoap import create_envelope, clean_namespaces
 from ...domain.proxy import Proxy
 from .ups_mapper import UPSMapper
 from .ups_client import UPSClient
@@ -12,18 +13,17 @@ class UPSProxy(Proxy):
         self.mapper = UPSMapper(client) if mapper is None else mapper
 
     def get_quotes(self, RateRequest_: Rate.FreightRateRequest):
-        envelope_ = _create_envelope(
-            header_content=self.mapper.Security,
-            body_content=RateRequest_
+        envelopeStr = export(
+            create_envelope(header_content=self.mapper.Security, body_content=RateRequest_),
+            namespacedef_='xmlns:tns="http://schemas.xmlsoap.org/soap/envelope/" xmlns:common="http://www.ups.com/XMLSchema/XOLTWS/Common/v1.0" xmlns:upss="http://www.ups.com/XMLSchema/XOLTWS/UPSS/v1.0" xmlns:wsf="http://www.ups.com/schema/wsf" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:frt="http://www.ups.com/XMLSchema/XOLTWS/FreightRate/v1.0"'
         )
-        xmlStr = _export_envolope(
-            envelope_, 
+        xmlStr = clean_namespaces(
+            envelopeStr, 
             envelope_prefix='tns:', 
             header_child_prefix='upss:',
             body_child_prefix='frt:',
             header_child_name='UPSSecurity',
-            body_child_name='FreightRateRequest',
-            namespacedef_='xmlns:tns="http://schemas.xmlsoap.org/soap/envelope/" xmlns:common="http://www.ups.com/XMLSchema/XOLTWS/Common/v1.0" xmlns:upss="http://www.ups.com/XMLSchema/XOLTWS/UPSS/v1.0" xmlns:wsf="http://www.ups.com/schema/wsf" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:frt="http://www.ups.com/XMLSchema/XOLTWS/FreightRate/v1.0"'
+            body_child_name='FreightRateRequest'
         )
         result = http(
             url='%s/FreightRate' % self.client.server_url, 
@@ -34,18 +34,17 @@ class UPSProxy(Proxy):
         return to_xml(result)
 
     def get_trackings(self, TrackRequest_: Track.TrackRequest):
-        envelope_ = _create_envelope(
-            header_content=self.mapper.Security,
-            body_content=TrackRequest_
+        envelopeStr = export(
+            create_envelope(header_content=self.mapper.Security, body_content=TrackRequest_), 
+            namespacedef_='xmlns:tns="http://schemas.xmlsoap.org/soap/envelope/" xmlns:upss="http://www.ups.com/XMLSchema/XOLTWS/UPSS/v1.0" xmlns:trk="http://www.ups.com/XMLSchema/XOLTWS/Track/v2.0" xmlns:common="http://www.ups.com/XMLSchema/XOLTWS/Common/v1.0"'
         )
-        xmlStr = _export_envolope(
-            envelope_, 
+        xmlStr = clean_namespaces(
+            envelopeStr, 
             envelope_prefix='tns:', 
             header_child_prefix='upss:',
             body_child_prefix='trk:',
             header_child_name='UPSSecurity',
-            body_child_name='TrackRequest', 
-            namespacedef_='xmlns:tns="http://schemas.xmlsoap.org/soap/envelope/" xmlns:upss="http://www.ups.com/XMLSchema/XOLTWS/UPSS/v1.0" xmlns:trk="http://www.ups.com/XMLSchema/XOLTWS/Track/v2.0" xmlns:common="http://www.ups.com/XMLSchema/XOLTWS/Common/v1.0"'
+            body_child_name='TrackRequest'
         )
         result = http(
             url='%s/Track' % self.client.server_url, 
