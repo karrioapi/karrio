1436807355ecaabd16a28abb21913cf2f9277f9d
Dan <danielkobina@gmail.com>
2018-09-04 10:16:08 -0400

    add missing mapper methods for shipment creation request (#10)
    * add missing mapper methods for shipment creation request
    
    * fix shipment parsing return type

--- openship/domain/mapper.py
+++ openship/domain/mapper.py
@@ -21,3 +21,11 @@ class Mapper:
     def parse_tracking_response(self, response) -> Tuple[List[E.tracking_details], List[E.Error]]:
         """ Create united API tracking result list from carrier xml response  """
         raise Exception("Not Supported")
+
+    def create_shipment_request(self, payload: E.shipment_request):
+        """ Create carrier specific shipment creation request xml data from payload """
+        raise Exception("Not Supported")
+
+    def parse_shipment_response(self, response) -> Tuple[E.shipment_details, List[E.Error]]:
+        """ Create united API shipment creation result from carrier xml response  """
+        raise Exception("Not Supported")
