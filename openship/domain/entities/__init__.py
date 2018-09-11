2fa78b2de44181e35cf3b7712476940cf4421749
Dan Kobina <danielkobina@gmail.com>
2018-09-10 23:01:30 -0400

    update generic types

--- openship/domain/entities/__init__.py
+++ openship/domain/entities/__init__.py
@@ -9,11 +9,6 @@ class Quote():
         """ Create a quote request payload """
         return quote_request(**quote_request_type(**args)._asdict())
 
-    @staticmethod
-    def parse(**args) -> quote_details:
-        """ create a quote details data """
-        return quote_details(**args)
-
 class Tracking():
     """ manage tracking operations """
 
@@ -21,11 +16,6 @@ class Tracking():
     def create(**args) -> quote_request:
         """ Create a tracking request payload """
         return tracking_request(**args)
-
-    @staticmethod
-    def parse(**args) -> tracking_details:
-        """ create a tracking details data """
-        return tracking_details(**args)
 
 class Shipment():
     """ manage shipment operations """
@@ -35,18 +25,3 @@ class Shipment():
         """ Create a shipment details request payload """
         return shipment_request(**shipment_request_type(**args)._asdict())
 
-    @staticmethod
-    def parse(**args) -> shipment_details:
-        """ create a shipment details data """
-        return shipment_details(**args)
-
-
-
-
-
-
-class Error():
-    def __init__(self, message: str = None, code: str = None, carrier: str = None):
-        self.message = message
-        self.code = code
-        self.carrier = carrier
