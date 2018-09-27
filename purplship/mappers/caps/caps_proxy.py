9177dad828e1c76628bc7613929a074ac61bb669
Dan Kobina <danielkobina@gmail.com>
2018-09-26 21:47:49 -0400

    implement pickup request, update and cancellation request for Canada post proxy

--- purplship/mappers/caps/caps_proxy.py
+++ purplship/mappers/caps/caps_proxy.py
@@ -1,7 +1,7 @@
 from io import StringIO
 from gds_helpers import export, to_xml, request as http
 from purplship.mappers.caps.caps_mapper import CanadaPostMapper, CanadaPostClient
-from pycaps import rating as Rate, track as Track
+from pycaps import rating as Rate, shipment as Ship, pickuprequest as Pick
 from purplship.domain.proxy import Proxy
 from base64 import b64encode
 
@@ -14,7 +14,9 @@ class CanadaPostProxy(Proxy):
         pair = "%s:%s" % (self.client.username, self.client.password)
         self.authorization = b64encode(pair.encode("utf-8")) 
 
-    def get_quotes(self, mailing_scenario: Rate.mailing_scenario):
+    """ Proxy interface methods """
+
+    def get_quotes(self, mailing_scenario: Rate.mailing_scenario) -> "XMLElement":
         xmlStr = export(
             mailing_scenario, 
             namespacedef_='xmlns="http://www.canadapost.ca/ws/ship/rate-v3"'
@@ -33,7 +35,77 @@ class CanadaPostProxy(Proxy):
         )
         return to_xml(result)
 
-    def get_trackings(self, tracking_pin: str):
+    def get_trackings(self, tracking_pin: str) -> "XMLElement":
+        """
+        get_trackings currently only get one tracking_pin
+        """
+        return self._get_tracking(tracking_pin)
+
+    def create_shipment(self, shipment: Ship.ShipmentType) -> "XMLElement":
+        xmlStr = export(
+            shipment, 
+            name_='shipment',
+            namespacedef_='xmlns="http://www.canadapost.ca/ws/shipment-v8"'
+        )
+        mail_on_behalf = self.client.customer_number
+
+        result = http(
+            url="%s/rs/%s/%s/shipment" % (self.client.server_url, self.client.customer_number, mail_on_behalf), 
+            data=bytearray(xmlStr, "utf-8"), 
+            headers={
+                'Content-Type': 'application/vnd.cpc.shipment-v8+xml',
+                'Accept': 'application/vnd.cpc.shipment-v8+xml',
+                'Authorization': "Basic %s" % self.authorization,
+                'Accept-language': 'en-CA'
+            }, 
+            method="POST"
+        )
+        return to_xml(result)
+
+    def request_pickup(self, pickup_request_details: Pick.PickupRequestDetailsType, method: str = "POST") -> "XMLElement":
+        xmlStr = export(
+            pickup_request_details, 
+            name_='pickup-request-details',
+            namespacedef_='xmlns="http://www.canadapost.ca/ws/pickuprequest"'
+        )
+        result = http(
+            url="%s/enab/%s/pickuprequest" % (self.client.server_url, self.client.customer_number), 
+            data=bytearray(xmlStr, "utf-8"), 
+            headers={
+                'Content-Type': 'application/vnd.cpc.pickuprequest+xml',
+                'Accept': 'application/vnd.cpc.pickuprequest+xml',
+                'Authorization': "Basic %s" % self.authorization,
+                'Accept-language': 'en-CA'
+            }, 
+            method="POST"
+        )
+        return to_xml(result)
+
+    def modify_pickup(self, pickup_request_details: Pick.PickupRequestDetailsType) -> "XMLElement":
+        """
+        pickup update is similar to pickup request except that it is a PUT
+        """
+        return self.request_pickup(pickup_request_details, "PUT")
+
+    def cancel_pickup(self, rel: str) -> "XMLElement":
+        """
+        Invoke the link returned from a prior call to Get Pickup Requests where rel= “self”
+        <link rel="self" .../>
+        """
+        result = http(
+            url=rel, 
+            headers={
+                'Accept': 'application/vnd.cpc.pickuprequest+xml',
+                'Authorization': "Basic %s" % self.authorization,
+                'Accept-language': 'en-CA'
+            }, 
+            method="DELETE"
+        )
+        return to_xml(result)
+
+    """ Canada Post Proxy interface methods """
+
+    def _get_tracking(self, tracking_pin: str) -> "XMLElement":
         result = http(
             url="%s/vis/track/pin/%s/summary" % (self.client.server_url, tracking_pin), 
             headers={
