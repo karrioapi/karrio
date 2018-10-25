48626bcc266a05e8a61dabfbcaa6e7b668566694
Dan Kobina <danielkobina@gmail.com>
2018-10-25 04:55:43 -0400

    update rc version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='purplship',
-      version='1.0-rc2',
+      version='1.0-rc3',
       description='Shipping carriers integration with python',
       long_description=long_description,
       long_description_content_type="text/markdown",
