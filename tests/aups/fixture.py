856547defcac34efa2a493e9d1699543dbe45dd6
Dan Kobina <danielkobina@gmail.com>
2019-03-01 11:49:06 -0500

    use gateway to initialize australia post fixture proxy

--- tests/aups/fixture.py
+++ tests/aups/fixture.py
@@ -1,10 +1,8 @@
-from purplship.mappers.aups import AustraliaPostClient, AustraliaPostProxy
+import purplship
 
-proxy = AustraliaPostProxy(
-    AustraliaPostClient(
-        server_url="https://digitalapi.auspost.com.au/test",
-        username="username",
-        password="password",
-        account_number="1234567",
-    )
-)
+proxy = purplship.gateway['aups'].create({
+    "server_url": "https://digitalapi.auspost.com.au/test",
+    "username": "username",
+    "password": "password",
+    "account_number": "1234567"
+})
