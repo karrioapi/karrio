973e3ae902fd15ec5a5a489da731430dd19d7c99
Dan Kobina <danielkobina@gmail.com>
2019-03-04 02:03:40 -0500

    introduce mapper boilerplate and template folder

---
+++ .template/mappers/carrier/carrier_mapper/partials/__init__.py
@@ -0,0 +1,4 @@
+from .rate import CarrierNameMapperPartial as CarrierNameQuoteMapperPartial
+from .track import CarrierNameMapperPartial as CarrierNameTrackMapperPartial
+from .shipment import CarrierNameMapperPartial as CarrierNameShipmentMapperPartial
+from .pickup import CarrierNameMapperPartial as CarrierNamePickupMapperPartial
---
+++ .template/mappers/carrier/carrier_units.py
@@ -0,0 +1,1 @@
+"""CarrierName Native Types/Units."""
