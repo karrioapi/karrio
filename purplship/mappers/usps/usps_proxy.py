wjshan/purplship

udpate typing to pass mypy check

purplship/mappers/usps/usps_proxy.py
@@ -1,9 +1,13 @@
 from io import StringIO
+from lxml import etree
 from typing import List, Union
 from gds_helpers import export, to_xml, request as http
 from purplship.mappers.usps.usps_mapper import USPSMapper
 from purplship.mappers.usps.usps_client import USPSClient
 from purplship.domain.proxy import Proxy
+from pyusps.RateV4Request import RateV4Request
+from pyusps.IntlRateV2Request import IntlRateV2Request
+from pyusps.TrackRequest import TrackRequest
 
 from functools import reduce
 
@@ -15,8 +19,8 @@ class USPSProxy(Proxy):
 
     """ Proxy interface methods """
 
-    def get_quotes(self, rate_request: '') -> "XMLElement":
+    def get_quotes(self, rate_request: Union[RateV4Request, IntlRateV2Request]) -> etree.ElementBase:
         pass
 
-    def get_trackings(self, tracking_request: '') -> "XMLElement":
+    def get_trackings(self, tracking_request: TrackRequest) -> etree.ElementBase:
         pass

karrioapi/karrio

purplship/mappers/usps/usps_proxy.py
@@ -1,9 +1,13 @@
 from io import StringIO
+from lxml import etree
 from typing import List, Union
 from gds_helpers import export, to_xml, request as http
 from purplship.mappers.usps.usps_mapper import USPSMapper
 from purplship.mappers.usps.usps_client import USPSClient
 from purplship.domain.proxy import Proxy
+from pyusps.RateV4Request import RateV4Request
+from pyusps.IntlRateV2Request import IntlRateV2Request
+from pyusps.TrackRequest import TrackRequest
 
 from functools import reduce
 
@@ -15,8 +19,8 @@ class USPSProxy(Proxy):
 
     """ Proxy interface methods """
 
-    def get_quotes(self, rate_request: '') -> "XMLElement":
+    def get_quotes(self, rate_request: Union[RateV4Request, IntlRateV2Request]) -> etree.ElementBase:
         pass
 
-    def get_trackings(self, tracking_request: '') -> "XMLElement":
+    def get_tracking(self, TrackingRequest: TrackRequest) -> etree.ElementBase:
         pass