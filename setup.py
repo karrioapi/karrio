fb7716eba44aca8b81340d29ccc80df9752a6120
DanH91 <danielkobina@gmail.com>
2020-04-19 17:54:22 -0400

    update packages versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='purplship',
-      version='2020.4.0',
+      version='2020.4.1',
       description='Multi-carrier shipping API integration with python',
       long_description=long_description,
       long_description_content_type="text/markdown",
