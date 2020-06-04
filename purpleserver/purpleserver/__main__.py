dd13834cccccd46e72338e166a475e2e65712d04
DanH91 <danielkobina@gmail.com>
2020-06-04 16:35:31 -0400

    remove  prefix env var

--- purpleserver/purpleserver/__main__.py
+++ purpleserver/purpleserver/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""PurplShip's command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -10,7 +10,7 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+            "Couldn't start PurplShip. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
