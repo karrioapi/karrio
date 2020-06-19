0c1eee083fd675fb9758a9726fd0b0fdb36ed6bd
DanH91 <danielkobina@gmail.com>
2020-06-19 00:17:45 -0400

    fix models registration to only register installed carriers

--- purpleserver/core/purpleserver/core/admin.py
+++ purpleserver/core/purpleserver/core/admin.py
@@ -1,7 +1,5 @@
-import inspect
 from django.contrib import admin
 import purpleserver.core.models as models
 
-for name, model in inspect.getmembers(models):
-    if inspect.isclass(model) and issubclass(model, models.Carrier) and name not in ['Carrier']:
-        admin.site.register(model)
+for name, model in models.MODELS.items():
+    admin.site.register(model)
