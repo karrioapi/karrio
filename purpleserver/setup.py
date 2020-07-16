e05d0a63f2a180151581f5d00c286b2189369902
DanH91 <danielkobina@gmail.com>
2020-07-16 00:24:58 -0400

    Bump version to 2020.7.1

--- purpleserver/setup.py
+++ purpleserver/setup.py
@@ -28,7 +28,7 @@ manager_requirements = [
 
 setup(
       name='purplship-server',
-      version='2020.7.0',
+      version='2020.7.1',
       description='Multi-carrier shipping API',
       long_description=long_description,
       long_description_content_type="text/markdown",
