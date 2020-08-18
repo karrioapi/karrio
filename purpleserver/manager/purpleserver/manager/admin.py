4c2dc8779a38aab3efdc52ea1085e9115a3627c0
DanH91 <danielkobina@gmail.com>
2020-08-18 06:25:27 -0400

    remove shipment manager data from admin site

--- purpleserver/manager/purpleserver/manager/admin.py
+++ purpleserver/manager/purpleserver/manager/admin.py
@@ -1,8 +1,1 @@
-import inspect
-from django.contrib import admin
-from purpleserver.core.models import Entity
-import purpleserver.manager.models as models
-
-for name, model in inspect.getmembers(models):
-    if inspect.isclass(model) and issubclass(model, Entity) and name not in ['Entity', 'OwnedEntity', 'Carrier']:
-        admin.site.register(model)
+# from django.contrib import admin
