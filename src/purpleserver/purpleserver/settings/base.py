d1118aca9b645d5d238aed8828eb0b85278a4039
Dan Kobina <danielkobina@gmail.com>
2020-09-09 02:33:45 -0400

    change logos and favicon to latest versions

--- src/purpleserver/purpleserver/settings/base.py
+++ src/purpleserver/purpleserver/settings/base.py
@@ -98,8 +98,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'purpleserver.urls'
 LOGOUT_REDIRECT_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/api' if importlib.util.find_spec('purpleserver.client') is None else '/'
 LOGIN_URL = '/login/'
+
 
 TEMPLATES = [
     {
