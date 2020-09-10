8ada27a4c7b10ae9bc6530ead41b4ccf2d6ffb92
Dan Kobina <danielkobina@gmail.com>
2020-09-09 21:43:41 -0400

    update eshipper to support multi-package and implement purpleserver module

--- purplship/eshipper/purplship/carriers/eshipper/__init__.py
+++
@@ -1,2 +0,0 @@
-from purplship.carriers.eshipper.quote import parse_quote_reply, quote_request
-from purplship.carriers.eshipper.shipping import parse_shipping_reply, shipping_request
---
+++ src/eshipper/purplship/providers/eshipper/__init__.py
@@ -0,0 +1,2 @@
+from purplship.providers.eshipper.quote import parse_quote_reply, quote_request
+from purplship.providers.eshipper.shipping import parse_shipping_reply, shipping_request
