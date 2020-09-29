b44551766eb30ab79ed12f8ad3334e3ba24febdd
Dan Kobina <danielkobina@gmail.com>
2020-09-29 13:37:55 -0400

    update dev requirements

--- src/purpleserver/setup.py
+++ src/purpleserver/setup.py
@@ -10,11 +10,13 @@ dev_requirements = [
       'wheel',
 
       'purplship.canadapost',
-      'purplship.dhl',
+      'purplship.dhl-express',
       'purplship.fedex',
-      'purplship.package',
+      'purplship.fedex-express',
       'purplship.purolator',
+      'purplship.purolator-courier',
       'purplship.ups',
+      'purplship.ups-package',
 
       'eshipper.extension',
       'freightcom.extension',
@@ -33,7 +35,7 @@ setup(
       packages=find_packages("."),
       install_requires=[
             'django',
-            'djangorestframework',
+            'djangorestframework==3.11.1',
             'djangorestframework-camel-case',
             'drf-yasg',
             'django-oauth-toolkit',
