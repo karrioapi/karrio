d4d8496ffeb305f018d25d4d680b0cb6c4921a97
Dan Kobina <danielkobina@gmail.com>
2020-10-08 16:15:26 -0400

    add canpar schemas and generated datatypes

---
+++ py-canpar/README.md
@@ -0,0 +1,9 @@
+# py-canpar
+
+Canpar Python Data Types generated from canpar schechma files with [generateDS](http://www.davekuhlman.org/generateDS.html) library
+
+## Installation
+
+```bash
+pip install -f https://git.io/purplship py-canpar
+```
---
+++ py-canpar/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='py-canpar',
+      version='2020.10.0',
+      description='Canpar Python Schemas Datatypes',
+      url='https://github.com/PurplShip/purplship-carriers/tree/master/py-canpar',
+      author='Purplship',
+      author_email='hello@purplship.com',
+      license='MIT',
+      packages=['pycanpar'],
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
