6aec7daa47989577e250e5e34c4ec19a5f6d01b4
Dan Kobina <danielkobina@gmail.com>
2020-10-09 19:01:34 -0400

    Bump patched components version

--- src/apps/client/setup.py
+++ src/apps/client/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='purplship-server.client',
-      version='2020.9.0',
+      version='2020.9.1',
       description='Multi-carrier shipping API client module',
       long_description=long_description,
       long_description_content_type="text/markdown",
