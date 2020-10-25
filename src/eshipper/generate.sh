5c28a4f9e329d1dd8f6045741a7007ff86c1d516
Dan Kobina <danielkobina@gmail.com>
2020-10-25 11:46:37 -0400

    generate schema datatypes for eshipper cancel shipement service

--- src/eshipper/generate.sh
+++ src/eshipper/generate.sh
@@ -3,3 +3,5 @@ generateDS --no-namespace-defs -o "./pyeshipper/quote_reply.py" ./vendor/schemas
 generateDS --no-namespace-defs -o "./pyeshipper/shipping_request.py" ./vendor/schemas/shipping_request.xsd
 generateDS --no-namespace-defs -o "./pyeshipper/shipping_reply.py" ./vendor/schemas/shipping_reply.xsd
 generateDS --no-namespace-defs -o "./pyeshipper/error.py" ./vendor/schemas/error.xsd
+generateDS --no-namespace-defs -o "./pyeshipper/shipment_cancel_request.py" ./vendor/schemas/shipment_cancel_request.xsd
+generateDS --no-namespace-defs -o "./pyeshipper/shipment_cancel_response.py" ./vendor/schemas/shipment_cancel_request.xsd
