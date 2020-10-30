185640105b5eb5522166de154c7c84ddbd8bed15
Dan Kobina <danielkobina@gmail.com>
2020-10-30 07:40:55 -0400

    initialize boxknight extension package

---
+++ extensions/boxknight/purplship/package/mappers/boxknight/__init__.py
@@ -0,0 +1,3 @@
+from purplship.package.mappers.boxknight.mapper import Mapper
+from purplship.package.mappers.boxknight.proxy import Proxy
+from purplship.package.mappers.boxknight.settings import Settings
---
+++ extensions/boxknight/purplship/providers/boxknight/units.py
@@ -0,0 +1,1 @@
+from enum import Flag, Enum
