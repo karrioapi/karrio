219b474b02d41bbbbbe46d6a7af002c46b703b5e
Dan Kobina <danielkobina@gmail.com>
2020-12-29 01:17:31 -0500

    enhance generic dropdown input abstraction

--- src/frontend/src/components/generic/dropdown-input.tsx
+++ src/frontend/src/components/generic/dropdown-input.tsx
@@ -1,26 +1,27 @@
-import { Reference } from '@/library/context';
 import { isNone } from '@/library/helper';
-import { Collection } from '@/library/types';
-import React, { useState, useRef, useContext, ChangeEvent, useEffect } from 'react';
+import React, { useState, useRef, ChangeEvent, useEffect } from 'react';
 
 
-interface DropdownInputComponent extends React.AllHTMLAttributes<HTMLInputElement> {
+export interface DropdownInputComponent extends React.AllHTMLAttributes<HTMLInputElement> {
     label?: string;
     fieldClass?: string;
     controlClass?: string;
     dropdownClass?: string;
-    onValueChange: (value: string | null) => void;
+    items?: [string, string][];
+    onValueChange: (value?: string | null) => void;
 }
 
 
-const DropdownInput: React.FC<DropdownInputComponent> = ({ label, defaultValue, fieldClass, controlClass, dropdownClass, required, onValueChange, ...props }) => {
+const DropdownInput: React.FC<DropdownInputComponent> = ({ label, name, items, defaultValue, fieldClass, controlClass, dropdownClass, required, onValueChange, ...props }) => {
     const btn = useRef<any>(null);
     const control = useRef<any>(null);
-    const Ref = useContext(Reference);
     const [key, setKey] = useState<string>(`dropdown-${Date.now()}`);
     const [isActive, setIsActive] = useState<boolean>(false);
     const [search, setSearch] = useState<string>("");
     const [value, setValue] = useState<string>();
+    const find = (value: string) => {
+        return items?.find(([key, val]) => key.toLowerCase() == value.toLowerCase() || val.toLowerCase() == value.toLowerCase())
+    };
     const handleOnClick = (e: React.MouseEvent) => {
         e.stopPropagation();
         e.preventDefault();
@@ -41,34 +42,36 @@ const DropdownInput: React.FC<DropdownInputComponent> = ({ label, defaultValue, 
             document.removeEventListener('click', onBodyClick);
         }
     };
-    const onInput = (e: ChangeEvent<any>) => {
+    const onSearch = (e: ChangeEvent<any>) => {
         setSearch(e.target.value as string);
     };
-    const onChange = (e: ChangeEvent<any>) => {
-        const code = Object.keys(Ref.countries).find(c => Ref.countries[c].toLowerCase() == e.target.value.toLowerCase());
-        setValue(code || "");
-        onValueChange(code as string);
+    const onRefChange = (e: ChangeEvent<any>) => {
+        e.preventDefault();
+        const [key] = find(e.target.value) || [];
+        setValue(key || "");
+        onValueChange(key as string);
     };
-    const onSelect = (code: string) => (e: React.MouseEvent) => {
-        setValue(code);
-        onValueChange(code);
+    const onSelect = (key: string) => (_: React.MouseEvent) => {
+        setValue(key);
+        onValueChange(key);
     };
 
     useEffect(() => {
-        if (!isNone(Ref?.countries) && !isNone(defaultValue)) {
-            setValue(Ref.countries[defaultValue as string]);
+        if (!isNone(items) && !isNone(defaultValue)) {
+            const [_, value] = find(defaultValue as string) || [];
+            setValue(value);
         }
-    }, [Ref?.countries, defaultValue]);
+    }, [items, defaultValue]);
 
     return (
-        <div className={`field ${fieldClass}`}>
+        <div className={`field ${fieldClass}`} key={key}>
             {label !== undefined && <label className="label is-capitalized" style={{ fontSize: ".8em" }}>
                 {label}
                 {required && <span className="icon is-small has-text-danger small-icon"><i className="fas fa-asterisk"></i></span>}
             </label>}
             <div className={`control ${controlClass}`}>
                 <div className={`dropdown select is-fullwidth ${isActive ? 'is-active' : ''} ${dropdownClass}`} key={`dropdown-input-`}>
-                    <input name="country" onChange={onChange} defaultValue={value} style={{ position: 'absolute', zIndex: -1 }}/>
+                    <input name={name} onChange={onRefChange} defaultValue={value} style={{ position: 'absolute', zIndex: -1 }} required={required}/>
                     <a onClick={handleOnClick} aria-haspopup="true" className="dropdown-trigger input is-fullwidth px-2" style={{ justifyContent: 'left' }} aria-controls={`dropdown-input-`} ref={btn}>
                         <span>{value}</span>
                     </a>
@@ -78,18 +81,17 @@ const DropdownInput: React.FC<DropdownInputComponent> = ({ label, defaultValue, 
 
                             <div className="panel-block px-1 py-1">
                                 <p className="control">
-                                    <input className="input" type="text" defaultValue={search} onInput={onInput} ref={control} />
+                                    <input className="input" type="text" defaultValue={search} onInput={onSearch} ref={control} />
                                 </p>
                             </div>
                             <nav className="panel dropped-panel">
-                                {Object
-                                    .entries(Ref?.countries as Collection || {})
-                                    .filter(([_, name]) => search === "" || name.toLowerCase().includes(search.toLowerCase()))
-                                    .map(([code, name]) => <>
-                                        <a className={`panel-block  ${code === value ? 'is-active' : ''}`} onClick={onSelect(code)} key={code} tabIndex={1}>
-                                            <span>{name}</span>
+                                {(items || [])
+                                    .filter(([_, val]) => search === "" || val.toLowerCase().includes(search.toLowerCase()))
+                                    .map(([key, val]) => (
+                                        <a key={`${key}-${Date.now()}`} onClick={onSelect(key)} className={`panel-block  ${key === value ? 'is-active' : ''}`}>
+                                            <span>{val}</span>
                                         </a>
-                                    </>)
+                                    ))
                                 }
                             </nav>
                         </div>
@@ -98,6 +100,6 @@ const DropdownInput: React.FC<DropdownInputComponent> = ({ label, defaultValue, 
             </div>
         </div>
     );
-}
+};
 
 export default DropdownInput;
