0096fb0c2c355494611686f3b84320461e802a80
Dan Kobina <danielkobina@gmail.com>
2021-02-09 11:04:56 -0500

    updated beta 'usps' purplship extension tracking integration only

--- extensions/usps/purplship/mappers/usps/proxy.py
+++ extensions/usps/purplship/mappers/usps/proxy.py
@@ -1,7 +1,4 @@
 import urllib.parse
-from typing import Union
-from usps_lib.rate_v4_request import RateV4Request
-from usps_lib.intl_rate_v2_request import IntlRateV2Request
 from usps_lib.track_field_request import TrackFieldRequest
 
 from purplship.api.proxy import Proxy as BaseProxy
@@ -13,13 +10,6 @@ class Proxy(BaseProxy):
     settings: Settings
 
     """ Proxy interface method implementations """
-
-    def get_rates(
-        self, request: Serializable[Union[RateV4Request, IntlRateV2Request]]
-    ) -> Deserializable[str]:
-        query = urllib.parse.urlencode(request.serialize())
-        response = http(url=f"{self.settings.server_url}?{query}", method="GET")
-        return Deserializable(response, XP.to_xml)
 
     def get_tracking(
         self, request: Serializable[TrackFieldRequest]
