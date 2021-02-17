44efe766fed924a806a73b34bcdeecea422362c4
Dan Kobina <danielkobina@gmail.com>
2021-02-17 07:54:50 -0500

    improve pricing module replacing PricingCharge by Surcharge and enrich help texts

--- src/apps/pricing/purpleserver/pricing/signals.py
+++ src/apps/pricing/purpleserver/pricing/signals.py
@@ -8,14 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 def register_rate_post_processing(*_, **__):
-    logger.debug("register custom charge  processing")
+    logger.debug("register custom surcharge  processing")
     try:
         Rates.post_process_functions = [
-            charge.apply_charge for charge in models.PricingCharge.objects.all()
+            charge.apply_charge for charge in models.Surcharge.objects.all()
         ]
     except:
         logger.warning("Failed to register custom charge processing")
 
 
-post_save.connect(register_rate_post_processing, sender=models.PricingCharge)
-post_delete.connect(register_rate_post_processing, sender=models.PricingCharge)
+post_save.connect(register_rate_post_processing, sender=models.Surcharge)
+post_delete.connect(register_rate_post_processing, sender=models.Surcharge)
