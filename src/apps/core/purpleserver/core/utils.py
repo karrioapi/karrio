cb7d5e9ffea809bead122b77a6697af4261ecfae
Dan Kobina <danielkobina@gmail.com>
2021-03-26 11:19:32 -0400

    introduce model relation update helpers

--- src/apps/core/purpleserver/core/utils.py
+++ src/apps/core/purpleserver/core/utils.py
@@ -1,6 +1,7 @@
 import functools
-from typing import Type, TypeVar, Generic, Optional, Union, Callable, Any, List, cast
+from typing import Type, TypeVar, Generic, Optional, Union, Callable, Any, List
 from rest_framework import serializers
+from django.db import models
 
 T = TypeVar('T')
 
@@ -47,7 +48,7 @@ class _SerializerDecoratorInitializer(Generic[T]):
 
     def __getitem__(self, serializer_type: Type[serializers.Serializer]):
         class Decorator:
-            def __init__(self, instance=None, data: Union[str, dict] = None):
+            def __init__(self, instance=None, data: Union[str, dict] = None, **kwargs):
                 self._instance = instance
 
                 if data is None and instance is None:
@@ -55,8 +56,8 @@ class _SerializerDecoratorInitializer(Generic[T]):
 
                 else:
                     self._serializer: serializer_type = (
-                        serializer_type(data=data) if instance is None else
-                        serializer_type(instance, data=data, partial=True)
+                        serializer_type(data=data, **kwargs) if instance is None else
+                        serializer_type(instance, data=data, **{**kwargs, 'partial': True})
                     )
 
                     self._serializer.is_valid(raise_exception=True)
@@ -79,3 +80,40 @@ class _SerializerDecoratorInitializer(Generic[T]):
 
 
 SerializerDecorator = _SerializerDecoratorInitializer()
+
+
+def save_many_to_many_data(
+        serializer: Type[serializers.ModelSerializer],
+        name: str,
+        instance: models.Model,
+        collection_data: List[dict] = None,
+        **kwargs):
+
+    current = getattr(instance, name, [])
+
+    if not any(collection_data) and any(current):
+        for item in current:
+            item.delete()
+
+    for data in collection_data:
+        item_instance = next((i for i in current if i.id == data.get('id')), None)
+        item = save_one_to_one_data(serializer, item_instance, data=data, **kwargs)
+
+        getattr(instance, name).add(item)
+
+
+def save_one_to_one_data(
+        serializer: Type[serializers.ModelSerializer],
+        instance: models.Model = None,
+        data: dict = None,
+        **kwargs):
+
+    if data is None and instance is not None:
+        instance.delete()
+        return None
+
+    if instance is None:
+        return SerializerDecorator[serializer](data=data, **kwargs).save().instance
+
+    return SerializerDecorator[serializer](instance=instance, data=data, **kwargs).save().instance
+
