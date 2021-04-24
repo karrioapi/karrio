6300f3e72d33a1ca703978e05d9f06404c7e3493
Dan Kobina <danielkobina@gmail.com>
2021-04-24 08:06:40 -0400

    extract webapp as a submodule and restructure the purplship server components

--- src/frontend/src/library/graphql.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-export const graphClient = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache({ addTypename: false })
-});
