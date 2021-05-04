66fd756e6b4ff72d82536d4973ca092a644ef4f4
Dan Kobina <danielkobina@gmail.com>
2021-05-04 07:33:43 -0400

    freeze dependency trees for release 2021.4

--- apps/client/setup.py
+++ apps/client/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='purplship-server.client',
-      version='2021.4-rc',
+      version='2021.4',
       description='Multi-carrier shipping API client module',
       long_description=long_description,
       long_description_content_type="text/markdown",
