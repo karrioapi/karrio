ba5344851dd33e1bc31f94b0ee5fc9073fcd1187
DanH91 <danielk.developer@gmail.com>
2021-05-12 10:32:40 -0400

    prepare patch release 2021.4.3

--- apps/graph/setup.py
+++ apps/graph/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='purplship-server.graph',
-      version='2021.4',
+      version='2021.4.1',
       description='Multi-carrier shipping API Graph module',
       long_description=long_description,
       long_description_content_type="text/markdown",
