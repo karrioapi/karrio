8a3b81d18c9356cd9b1826b381a17bf4d13eff5a
DanH91 <danielk.developer@gmail.com>
2021-05-14 05:30:31 -0400

    improve and docker image onboarding

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -8,36 +8,7 @@ else
 fi
 purplship collectstatic --clear --noinput || exit
 
-if [[ "$MULTI_TENANT_ENABLE" == "True" ]];
-then
-	(echo "
-from purpleserver.tenants.models import Client, Domain
-if not any(Client.objects.all()):
-  Domain.objects.create(domain='admin.purplship.com', tenant=Client.objects.create(name='public', schema_name='public'))
-  Domain.objects.create(domain='app.purplship.com', tenant=Client.objects.create(name='purplship', schema_name='purplship'))
-" | purplship shell) > /dev/null 2>&1;
-
-    (echo "
-from django_tenants.utils import tenant_context
-from django.contrib.auth import get_user_model
-from purpleserver.tenants.models import Client
-with tenant_context(Client.objects.get(schema_name='public')):
-  if not any(get_user_model().objects.all()):
-     get_user_model().objects.create_superuser('${TENANT_ADMIN_EMAIL}', '${TENANT_ADMIN_PASSWORD}')
-with tenant_context(Client.objects.get(schema_name='purplship')):
-  if not any(get_user_model().objects.all()):
-     get_user_model().objects.create_superuser('${ADMIN_EMAIL}', '${ADMIN_PASSWORD}')
-" | purplship shell) > /dev/null 2>&1;
-
-else
-  (echo "
-from purpleserver.user.models import User;
-if not any(User.objects.all()):
-  User.objects.create_superuser('${ADMIN_EMAIL}', '${ADMIN_PASSWORD}')
-" | purplship shell) > /dev/null 2>&1;
-fi
-
-
+# Start services
 set -m # turn on bash's job control
 
 gunicorn --config gunicorn-cfg.py purpleserver.wsgi &
