f30d8d9b2c9eda4b6169e80ebd3c89e5363e07a1
DanH91 <danielk.developer@gmail.com>
2021-05-28 04:52:51 -0400

    apply webapp parcel form patch

--- apps/events/setup.py
+++ apps/events/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='purplship-server.events',
-      version='2021.5-rc1',
+      version='2021.5-rc3',
       description='Multi-carrier shipping API Events module',
       long_description=long_description,
       long_description_content_type="text/markdown",
