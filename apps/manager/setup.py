7666a258f1ce157af45f5a44d71ec1057874e82a
DanH91 <danielk.developer@gmail.com>
2021-06-01 06:50:36 -0400

    setup version bumps for purplship server 2021.6 release candidate

--- apps/manager/setup.py
+++ apps/manager/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='purplship-server.manager',
-      version='2021.5',
+      version='2021.6-rc',
       description='Multi-carrier shipping API Shipments manager module',
       long_description=long_description,
       long_description_content_type="text/markdown",
