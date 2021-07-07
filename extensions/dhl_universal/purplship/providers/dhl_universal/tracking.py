132545b208868eadc43d99fb9898162506dd985d
DanH91 <danielk.developer@gmail.com>
2021-07-06 23:26:10 -0400

    use json mapping for dhl universal tracking response parsing

--- extensions/dhl_universal/purplship/providers/dhl_universal/tracking.py
+++ extensions/dhl_universal/purplship/providers/dhl_universal/tracking.py
@@ -1,7 +1,6 @@
 from typing import List, Tuple
 from dhl_universal_lib.tracking import (
     TrackingRequest as DHLTrackingRequest,
-    Shipment,
 )
 from purplship.core.utils import (
     Serializable,
@@ -21,35 +20,35 @@ from purplship.providers.dhl_universal.error import parse_error_response
 def parse_tracking_response(response: List[dict], settings: Settings) -> Tuple[List[TrackingDetails], List[Message]]:
     errors = [e for e in response if 'shipments' not in e]
     details = [
-        _extract_detail(Shipment(**d['shipments'][0]), settings)
+        _extract_detail(d['shipments'][0], settings)
         for d in response if 'shipments' in d
     ]
 
     return details, parse_error_response(errors, settings)
 
 
-def _extract_detail(detail: Shipment, settings: Settings) -> TrackingDetails:
+def _extract_detail(detail: dict, settings: Settings) -> TrackingDetails:
     return TrackingDetails(
         carrier_name=settings.carrier_name,
         carrier_id=settings.carrier_id,
 
-        tracking_number=str(detail.id),
+        tracking_number=str(detail.get('id')),
         events=[
             TrackingEvent(
-                date=DF.fdate(event.timestamp, '%Y-%m-%dT%H:%M:%S'),
-                description=event.description,
+                date=DF.fdate(event.get('timestamp'), '%Y-%m-%dT%H:%M:%S'),
+                description=event.get('description'),
                 location=(
-                    event.location.address.addressLocality
-                    if event.location is not None and event.location.address is not None
+                    event['location']['address'].get('addressLocality')
+                    if event.get('location') is not None and event.get('location').get('address') is not None
                     else None
                 ),
-                code=event.statusCode,
-                time=DF.ftime(event.timestamp, '%Y-%m-%dT%H:%M:%S'),
-            ) for event in detail.events
+                code=event.get('statusCode'),
+                time=DF.ftime(event.get('timestamp'), '%Y-%m-%dT%H:%M:%S'),
+            ) for event in detail.get('events') or []
         ],
         delivered=(
-            detail.status.status.lower() == "delivered"
-            if all([detail.status, detail.status.status]) else
+            detail['status']['status'].lower() == "delivered"
+            if all([detail['status'], detail['status']['status']]) else
             False
         )
     )
