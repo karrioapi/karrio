7f65bd29db39110a8f17b4509ed34b2b8aa31c05
DanH91 <danielk.developer@gmail.com>
2021-07-08 06:48:23 -0400

    make PURPLSHIP_HOST configuration through env var

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,9 +1,10 @@
 # -*- encoding: utf-8 -*-
 import decouple
 
+PURPLSHIP_HOST = decouple.config("PURPLSHIP_HOST", default="0.0.0.0")
 PURPLSHIP_PORT = decouple.config("PURPLSHIP_PORT", default=5002)
 
-bind = f'0.0.0.0:{PURPLSHIP_PORT}'
+bind = f'{PURPLSHIP_HOST}:{PURPLSHIP_PORT}'
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
