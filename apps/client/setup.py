7e18aabb59bad5ddcff9dc87b11cebc769e9f93f
DanH91 <danielk.developer@gmail.com>
2021-07-08 18:33:15 -0400

    release 2021.6.3 patch

--- apps/client/setup.py
+++ apps/client/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='purplship-server.client',
-      version='2021.6.2',
+      version='2021.6.3',
       description='Multi-carrier shipping API client module',
       long_description=long_description,
       long_description_content_type="text/markdown",
