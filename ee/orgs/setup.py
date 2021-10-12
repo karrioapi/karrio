7496dd55c0c6ac04ab764d13ffa57372c3b48868
DanH91 <danielk.developer@gmail.com>
2021-10-11 20:04:35 -0400

    prepare release version 2021.10rc0

--- ee/orgs/setup.py
+++ ee/orgs/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='purplship.server.orgs',
-      version='2021.8',
+      version='2021.10rc',
       description='Multi-carrier shipping API organization module',
       long_description=long_description,
       long_description_content_type="text/markdown",
--- packages/client/setup.py
+++ packages/client/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='purplship.server.client',
-      version='2021.8',
+      version='2021.10rc',
       description='Multi-carrier shipping API client module',
       long_description=long_description,
       long_description_content_type="text/markdown",
