42ca94dac35d4633ffc7710bfb7ef1e89673bcee
DanH91 <danielk.developer@gmail.com>
2021-10-15 02:00:13 -0400

    release 2021.10rc1

--- ee/tenants/setup.py
+++ ee/tenants/setup.py
@@ -4,26 +4,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-      name='purplship.server.tenants',
-      version='2021.10rc',
-      description='Multi-carrier shipping API muti-tenant module',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/PurplShip/purplship-server',
-      author='purplship',
-      author_email='danielk.developer@gmail.com',
-      license='Apache License Version 2.0',
-      packages=find_namespace_packages("."),
-      install_requires=[
-            'purplship.server.core',
-            'django-tenants',
-      ],
-      dependency_links=[
-            'https://git.io/purplship',
-      ],
-      classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: Apache Software License",
-      ],
-      zip_safe=False
+    name="purplship.server.tenants",
+    version="2021.10rc1",
+    description="Multi-carrier shipping API muti-tenant module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PurplShip/purplship-server",
+    author="purplship",
+    author_email="danielk.developer@gmail.com",
+    license="Apache License Version 2.0",
+    packages=find_namespace_packages("."),
+    install_requires=[
+        "purplship.server.core",
+        "django-tenants",
+    ],
+    dependency_links=[
+        "https://git.io/purplship",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    zip_safe=False,
 )
--- packages/core/setup.py
+++ packages/core/setup.py
@@ -4,27 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-      name='purplship.server.core',
-      version='2021.10rc',
-      description='Multi-carrier shipping API Core module',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/purplship/purplship-server',
-      author='purplship',
-      author_email='danielk.developer@gmail.com',
-      license='Apache License Version 2.0',
-      packages=find_namespace_packages(),
-      install_requires=[
-            'purplship',
-            'psycopg2-binary',
-      ],
-      dependency_links=[
-            'https://git.io/purplship',
-      ],
-      classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: Apache Software License",
-      ],
-      zip_safe=False,
-      include_package_data=True
+    name="purplship.server.core",
+    version="2021.10rc1",
+    description="Multi-carrier shipping API Core module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/purplship/purplship-server",
+    author="purplship",
+    author_email="danielk.developer@gmail.com",
+    license="Apache License Version 2.0",
+    packages=find_namespace_packages(),
+    install_requires=[
+        "purplship",
+        "psycopg2-binary",
+    ],
+    dependency_links=[
+        "https://git.io/purplship",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    zip_safe=False,
+    include_package_data=True,
 )
