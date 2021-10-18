281136e13e6aaba399665fe26dcc7cdcc547f767
DanH91 <danielk.developer@gmail.com>
2021-10-18 10:28:02 -0400

    prepare updated libs for release

--- dhl_express/generate.sh
+++ dhl_express/generate.sh
@@ -1,12 +1,12 @@
 SCHEMAS=./schemas
-LIB_MODULES=./dhl_express
+LIB_MODULES=./dhl_express_lib
 mkdir -p $LIB_MODULES
 find "${LIB_MODULES}" -name "*.py" -exec rm -r {} \;
 touch "${LIB_MODULES}/__init__.py"
 
 quicktype () {
     echo "Generating $1..."
-    docker run -it -v $PWD:/app -e SCHEMAS=/app/schemas -e LIB_MODULES=/app/dhl_express \
+    docker run -it -v $PWD:/app -e SCHEMAS=/app/schemas -e LIB_MODULES=/app/dhl_express_lib \
     purplship/tools /quicktype/script/quicktype --no-uuids --no-date-times --no-enums --src-lang json --lang jstruct \
     --no-nice-property-names --all-properties-optional $@
 }
