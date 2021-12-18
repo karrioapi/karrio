42a2c1d8d7720b945fba579f427e95fd9c137fe4
DanH91 <danielk.developer@gmail.com>
2021-12-18 08:47:01 -0500

    introduced universal shipment label generator integration

--- sdk/core/purplship/universal/providers/__init__.py
+++ sdk/core/purplship/universal/providers/__init__.py
@@ -1,1 +1,5 @@
 from purplship.universal.providers.rate import parse_rate_response, rate_request
+from purplship.universal.providers.shipment import (
+    parse_shipment_response,
+    shipment_request,
+)
