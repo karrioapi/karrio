f69d466cf9c2051e66194b645845105e3c555c17
DanH91 <danielk.developer@gmail.com>
2022-01-29 16:33:09 -0500

    increase access token lifetime until refresh cycle is improved in the dashboard

--- server/main/purplship/server/settings/base.py
+++ server/main/purplship/server/settings/base.py
@@ -275,8 +275,8 @@ REST_FRAMEWORK = {
 
 # JWT config
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=6),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
