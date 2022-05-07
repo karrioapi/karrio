ff1e12720126a5bb26aca2d15461e7546335a7a6
Daniel K <danielk.developer@gmail.com>
2022-05-07 05:07:36 -0400

    remove migration dependencies on amazon mws rollback

--- server/modules/core/karrio/server/providers/migrations/0031_delete_amazonmwssettings.py
+++ server/modules/core/karrio/server/providers/migrations/0031_delete_amazonmwssettings.py
@@ -6,14 +6,13 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pricing', '0023_auto_20220504_1335'),
-        ('manager', '0033_auto_20220504_1335'),
-        ('orgs', '0005_auto_20211231_2353'),
-        ('providers', '0030_amazonmwssettings'),
+        ("pricing", "0023_auto_20220504_1335"),
+        ("manager", "0033_auto_20220504_1335"),
+        ("providers", "0030_amazonmwssettings"),
     ]
 
     operations = [
         migrations.DeleteModel(
-            name='AmazonMwsSettings',
+            name="AmazonMwsSettings",
         ),
     ]
