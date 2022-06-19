82108ab4ab2395de68e1c391fe6944d0ce7dab53
Daniel K <danielk.developer@gmail.com>
2022-06-19 10:20:25 -0700

    setup TFA base with django-two-factor package

--- server/main/karrio/server/urls/__init__.py
+++ server/main/karrio/server/urls/__init__.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.contrib.auth.models import Group
 from django.conf import settings
 from django.urls import include, path
+from django.contrib import admin
+from django.contrib.auth.models import Group
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from constance.admin import Config
 
