6db42d7c0efafcdcdc82fef225a2fa332cc701a2
Daniel K <danielk.developer@gmail.com>
2022-06-28 04:26:09 -0700

    add basic model fields filters for audit log

--- insiders/server/audit/karrio/server/audit/hooks.py
+++ insiders/server/audit/karrio/server/audit/hooks.py
@@ -7,6 +7,12 @@ def register_model(model: models.Model) -> models.Model:
 
     exclude_fields = ["link"]
 
+    if "Parcel" in model.__name__:
+        exclude_fields += ["reference_number"]
+
+    if "Shipment" in model.__name__:
+        exclude_fields += ["tracking_url"]
+
     auditlog.register(model, exclude_fields=exclude_fields)
 
     return model
