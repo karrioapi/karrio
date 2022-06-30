1aef8383b382704ed95d01a8f212ba187568b32e
Daniel K <danielk.developer@gmail.com>
2022-06-30 11:13:50 -0700

    version freeze for karrio 2022.4.6

--- server/modules/manager/setup.py
+++ server/modules/manager/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="karrio.server.manager",
-    version="2022.4.3",
+    version="2022.4.6",
     description="Multi-carrier shipping API Shipments manager module",
     long_description=long_description,
     long_description_content_type="text/markdown",
