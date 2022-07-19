366e8811ad92e8094427b60d0ce2c5e9e61ad96a
Daniel K <danielk.developer@gmail.com>
2022-07-19 15:34:11 -0700

    make tracing admin data readonly

--- server/modules/core/karrio/server/tracing/admin.py
+++ server/modules/core/karrio/server/tracing/admin.py
@@ -3,11 +3,18 @@ from karrio.server.tracing import models
 
 
 class TracingRecordAdmin(admin.ModelAdmin):
-    readonly_fields = [f.name for f in models.TracingRecord._meta.get_fields()]
+    readonly_fields = [
+        f.name
+        for f in models.TracingRecord._meta.get_fields()
+        if f.name not in ["org", "link"]
+    ]
 
     def get_queryset(self, request):
         query = super().get_queryset(request)
         return query.filter(id__in=models.TracingRecord.access_by(request.user))
+
+    def has_add_permission(self, request) -> bool:
+        return False
 
 
 admin.site.register(models.TracingRecord, TracingRecordAdmin)
