b1fc90f79076cfc92ffa04d7b2a6573fcb7e2a16
Iyed Zairi <iyed.zairi@gmail.com>
2022-08-12 19:40:35 +0100

    Resolved issues

--- sdk/extensions/chronopost/karrio/providers/chronopost/__init__.py
+++ sdk/extensions/chronopost/karrio/providers/chronopost/__init__.py
@@ -1,4 +1,5 @@
 from karrio.providers.chronopost.utils import Settings
+from karrio.providers.chronopost.rate import rate_request, parse_rate_response
 from karrio.providers.chronopost.shipment import (
     parse_shipment_cancel_response,
     parse_shipment_response,
