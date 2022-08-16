a9a50d0b1da46a08ce363c2c3f85483fccb633f4
Iyed Zairi <iyed.zairi@gmail.com>
2022-08-16 19:45:00 +0100

    Add Chronopost settings database model to karrio server

---
+++ server/modules/core/karrio/server/providers/extension/models/chronopost.py
@@ -0,0 +1,20 @@
+from django.db import models
+from karrio.server.providers.models.carrier import Carrier, COUNTRIES
+
+
+class ChronopostSettings(Carrier):
+    class Meta:
+        db_table = "chronopost-settings"
+        verbose_name = 'Chronopost Settings'
+        verbose_name_plural = 'Chronopost Settings'
+
+    password = models.CharField(max_length=50)
+    account_number = models.CharField(max_length=50, blank=True, default='')
+    account_country_code = models.CharField(max_length=3, blank=True, choices=COUNTRIES)
+
+    @property
+    def carrier_name(self) -> str:
+        return 'chronopost'
+
+
+SETTINGS = ChronopostSettings
