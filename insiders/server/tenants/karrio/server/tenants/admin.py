8efed8eda284d25622bfb3767cdd8cb16b575109
Daniel K <danielk.developer@gmail.com>
2022-09-10 20:57:56 -0700

    add default feature flags for new tenant client

--- insiders/server/tenants/karrio/server/tenants/admin.py
+++ insiders/server/tenants/karrio/server/tenants/admin.py
@@ -1,11 +1,10 @@
 from django.contrib.admin import AdminSite, ModelAdmin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import GroupAdmin
 from django_tenants.admin import TenantAdminMixin
 from constance.admin import ConstanceAdmin, Config
 
-from karrio.server.user.admin import UserAdmin
+import karrio.server.conf as conf
+import karrio.server.user.admin as user_admin
 import karrio.server.tenants.models as models
 
 
@@ -18,11 +17,20 @@ class TenantsAdmin(AdminSite):
 class ClientAdmin(TenantAdminMixin, ModelAdmin):
     list_display = ("name",)
 
+    def get_form(self, request, obj, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["feature_flags"].initial = {
+            **conf.FEATURE_FLAGS,
+            "ORG_LEVEL_BILLING": False,
+            "TENANT_LEVEL_BILLING": False,
+        }
+        return form
+
 
 site = TenantsAdmin(name="system")
-site.register(get_user_model(), UserAdmin)
-site.register(Group, GroupAdmin)
-site.register(models.Client, ClientAdmin)
 site.register(models.Domain)
+site.register(models.Client, ClientAdmin)
+site.register(get_user_model(), user_admin.UserAdmin)
+site.register(user_admin.Group, user_admin.GroupAdmin)
 
 site.register([Config], ConstanceAdmin)
