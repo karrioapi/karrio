78326b2a618c066c2a4d5ac2cf4079b8765ee74e
Daniel K <danielk.developer@gmail.com>
2022-09-25 11:25:25 -0700

    (fix) drf-spectacular schema enum warnings

--- insiders/server/data/karrio/server/settings/data.py
+++ insiders/server/data/karrio/server/settings/data.py
@@ -1,3 +1,4 @@
+# type: ignore
 from karrio.server.settings.base import *
 
 
@@ -7,3 +8,8 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 IMPORT_EXPORT_SKIP_ADMIN_LOG = True
 IMPORT_EXPORT_CHUNK_SIZE = 200
 IMPORT_EXPORT_TMP_STORAGE_CLASS = "import_export.tmp_storages.TempFolderStorage"
+
+SPECTACULAR_SETTINGS["ENUM_NAME_OVERRIDES"] = {
+    **SPECTACULAR_SETTINGS["ENUM_NAME_OVERRIDES"],
+    "BatchOperationStatus": "karrio.server.data.serializers.OPERATION_STATUS",
+}
