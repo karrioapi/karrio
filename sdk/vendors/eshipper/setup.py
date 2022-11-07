afce8a15af252cd74927992bc3dea1e6134e103c
Daniel K <danielk.developer@gmail.com>
2022-11-07 12:44:55 -0800

    (fix) hs_code mapping for freightcom and eshipper

--- sdk/vendors/eshipper/setup.py
+++ sdk/vendors/eshipper/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="karrio.eshipper",
-    version="2022.8.6",
+    version="2022.8.7",
     description="Karrio - eShipper Shipping extension",
     long_description=long_description,
     long_description_content_type="text/markdown",
