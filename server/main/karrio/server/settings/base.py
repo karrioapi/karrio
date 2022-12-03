148cca3b3b62181bfa96213d6eb2b97691649adc
Daniel K <danielk.developer@gmail.com>
2022-12-02 20:56:23 -0800

    (increase) session lifetime until the dashboard gets better handle

--- server/main/karrio/server/settings/base.py
+++ server/main/karrio/server/settings/base.py
@@ -372,7 +372,7 @@ REST_FRAMEWORK = {
 
 # JWT config
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
