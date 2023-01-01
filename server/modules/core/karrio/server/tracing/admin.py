9fe53d6ab31d3d7ef118453ffc3ed7da7dd9e1b7
Daniel K <danielk.developer@gmail.com>
2022-12-31 23:37:19 -0800

    (improve) tracing filtering and search

--- server/modules/core/karrio/server/tracing/admin.py
+++ server/modules/core/karrio/server/tracing/admin.py
@@ -9,9 +9,9 @@ from karrio.server.tracing import models
 
 
 class TracingRecordAdmin(admin.ModelAdmin):
-    list_display = ('id', 'log', 'key', 'test_mode', 'request_timestamp', 'created_at')
-    search_fields = ('meta__request_log_id',)
-    list_filter = ('key', 'test_mode')
+    list_display = ("id", "log", "key", "test_mode", "request_timestamp", "created_at")
+    search_fields = ("meta__request_log_id", "meta__carrier_name")
+    list_filter = ("key", "test_mode")
     readonly_fields = [
         f.name
         for f in models.TracingRecord._meta.get_fields()
@@ -20,19 +20,26 @@ class TracingRecordAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         query = super().get_queryset(request)
-        return query.filter(id__in=models.TracingRecord.access_by(request.user)).order_by('-timestamp')
+        return query.filter(
+            id__in=models.TracingRecord.access_by(request.user)
+        ).order_by("-timestamp")
 
     def has_add_permission(self, request) -> bool:
         return False
 
     def log(self, obj):
-        log_id = obj.meta.get('request_log_id')
+        log_id = obj.meta.get("request_log_id")
 
         if any(str(log_id)):
-            return mark_safe('<a href="{}">{}</a>'.format(
-                reverse(f"admin:{APIRequestLog._meta.app_label}_{APIRequestLog._meta.model_name}_change", args=(log_id,)),
-                log_id
-            ))
+            return mark_safe(
+                '<a href="{}">{}</a>'.format(
+                    reverse(
+                        f"admin:{APIRequestLog._meta.app_label}_{APIRequestLog._meta.model_name}_change",
+                        args=(log_id,),
+                    ),
+                    log_id,
+                )
+            )
 
         return ""
 
@@ -40,10 +47,11 @@ class TracingRecordAdmin(admin.ModelAdmin):
         timestamp = datetime.datetime.fromtimestamp(obj.timestamp)
 
         if timestamp:
-            return timestamp.strftime('%Y-%m-%d %H:%M:%S')
+            return timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
-        return ''
+        return ""
 
-    request_timestamp.admin_order_field = 'timestamp'
+    request_timestamp.admin_order_field = "timestamp"
+
 
 admin.site.register(models.TracingRecord, TracingRecordAdmin)
