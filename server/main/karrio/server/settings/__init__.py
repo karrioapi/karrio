6d16d14641507e9a62a2250b7425237c67594e3c
Daniel K <danielk.developer@gmail.com>
2023-01-28 09:54:19 -0800

    (fix) redis health check

--- server/main/karrio/server/settings/__init__.py
+++ server/main/karrio/server/settings/__init__.py
@@ -3,12 +3,12 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 import importlib.util
 from karrio.server.settings.base import *
-from karrio.server.settings.email import *
-from karrio.server.settings.constance import *
+from karrio.server.settings.apm import *
 from karrio.server.settings.cache import *
 from karrio.server.settings.workers import *
-from karrio.server.settings.apm import *
 from karrio.server.settings.debug import *
+from karrio.server.settings.email import *
+from karrio.server.settings.constance import *
 
 
 if importlib.util.find_spec("karrio.server.graph") is not None:
