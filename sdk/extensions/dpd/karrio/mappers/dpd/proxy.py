69e8b4542e3acb8bc81d5ede4f3b7039a79dd6a8
Daniel K <danielk.developer@gmail.com>
2023-03-09 16:49:32 -0800

    (rename) dpd_belux to dpd extension

---
+++ sdk/extensions/dpd/karrio/mappers/dpd/proxy.py
@@ -0,0 +1,43 @@
+
+"""Karrio DPD client proxy."""
+
+import karrio.lib as lib
+import karrio.api.proxy as proxy
+import karrio.mappers.dpd.settings as provider_settings
+
+
+class Proxy(proxy.Proxy):
+    settings: provider_settings.Settings
+    def create_shipment(self, request: lib.Serializable) -> lib.Deserializable[str]:
+        response = lib.request(
+            url=f"",
+            data=request.serialize(),
+            trace=self.trace_as("xml"),
+            method="POST",
+            headers={},
+        )
+
+        return lib.Deserializable(response, lib.to_element)
+    
+    def cancel_shipment(self, request: lib.Serializable) -> lib.Deserializable[str]:
+        response = lib.request(
+            url=f"",
+            data=request.serialize(),
+            trace=self.trace_as("xml"),
+            method="POST",
+            headers={},
+        )
+
+        return lib.Deserializable(response, lib.to_element)
+    
+    def get_tracking(self, request: lib.Serializable) -> lib.Deserializable[str]:
+        response = lib.request(
+            url=f"",
+            data=request.serialize(),
+            trace=self.trace_as("xml"),
+            method="POST",
+            headers={},
+        )
+
+        return lib.Deserializable(response, lib.to_element)
+    
--- sdk/extensions/dpd_belux/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from tests.dpd_belux import *
