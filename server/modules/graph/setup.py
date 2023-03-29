68db0e3664664b80f8d877ce23bed2832bc90a90
Daniel K <danielk.developer@gmail.com>
2023-03-29 06:48:13 -0700

    (release) patch 2023.3.4

--- server/modules/graph/setup.py
+++ server/modules/graph/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="karrio.server.graph",
-    version="2023.3",
+    version="2023.3.4",
     description="Multi-carrier shipping API Graph module",
     long_description=long_description,
     long_description_content_type="text/markdown",
