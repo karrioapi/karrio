45015fe05bae1e31b59aedf3c0c6c41d6e59d2c3
Daniel K <daniel@thequantuminsider.com>
2023-08-22 08:53:43 -0700

    (revert) schemas to setup.py format

--- schemas/tnt/setup.py
+++ schemas/tnt/setup.py
@@ -1,12 +1,26 @@
 """Warning: This setup.py is only there for git install until poetry support git subdirectory"""
-
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="carrier.tnt",
-    version="0.0.0-dev",
+    version="2021.4",
+    description="TNT Web API Schemas Python Data Types library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/karrioapi/karrio",
+    author="Karrio",
+    author_email="hello@karrio.io",
     license="Apache-2.0",
     packages=find_packages(),
     install_requires=["six", "lxml"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
     zip_safe=False,
+    include_package_data=True,
 )
