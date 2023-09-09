724a6cf5baa0a0a441b96d27f168926aedc40d8f
Daniel K <danielk.developer@gmail.com>
2023-09-08 21:32:54 -0700

    (add) Locate2u database model

---
+++ server/modules/core/karrio/server/providers/extension/models/locate2u.py
@@ -0,0 +1,29 @@
+import django.db.models as models
+import django.core.cache as caching
+
+import karrio.lib as lib
+import karrio.server.providers.models.carrier as providers
+
+
+class Locate2uSettings(providers.Carrier):
+    class Meta:
+        db_table = "locate2u-settings"
+        verbose_name = "Locate2u Settings"
+        verbose_name_plural = "Locate2u Settings"
+
+    client_id = models.CharField(max_length=200)
+    client_secret = models.CharField(max_length=200)
+    account_country_code = models.CharField(
+        max_length=3, blank=True, null=True, choices=providers.COUNTRIES
+    )
+
+    @property
+    def carrier_name(self) -> str:
+        return "locate2u"
+
+    @property
+    def cache(self):
+        return lib.Cache(cache=caching.cache)
+
+
+SETTINGS = Locate2uSettings
