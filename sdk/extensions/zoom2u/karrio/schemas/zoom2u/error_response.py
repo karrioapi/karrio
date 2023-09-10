b350a43949c46347ba3f94a0b19dbd7046c14f25
Daniel K <danielk.developer@gmail.com>
2023-09-10 04:29:05 -0700

    (fix) error data schema to handle dynamic modelState data

--- sdk/extensions/zoom2u/karrio/schemas/zoom2u/error_response.py
+++ sdk/extensions/zoom2u/karrio/schemas/zoom2u/error_response.py
@@ -1,16 +1,10 @@
 from attr import s
-from typing import List, Optional
-from jstruct import JStruct
-
-
-@s(auto_attribs=True)
-class ModelStateType:
-    getQuoteRequestPickupSuburb: List[str] = []
-    getQuoteRequestPickupPostcode: List[str] = []
+from typing import Optional, Any
+from jstruct import JDict
 
 
 @s(auto_attribs=True)
 class ErrorResponseType:
     errorcode: Optional[str] = None
     message: Optional[str] = None
-    modelState: Optional[ModelStateType] = JStruct[ModelStateType]
+    modelState: Optional[dict] = JDict[str, Any]
