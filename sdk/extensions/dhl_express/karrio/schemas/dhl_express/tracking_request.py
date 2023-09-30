ad3025125812cb7ee7c71f26c5922265b4eb802b
Daniel K <danielk.developer@gmail.com>
2023-09-29 23:41:56 -0700

    (update) dhl_express schemas

--- sdk/extensions/dhl_express/karrio/schemas/dhl_express/tracking_request.py
+++ sdk/extensions/dhl_express/karrio/schemas/dhl_express/tracking_request.py
@@ -4,11 +4,11 @@ from typing import List, Optional
 
 @s(auto_attribs=True)
 class TrackingRequestType:
-    shipmentTrackingNumber: List[int] = []
-    pieceTrackingNumber: List[int] = []
-    shipmentReference: Optional[int] = None
+    shipmentTrackingNumber: List[str] = []
+    pieceTrackingNumber: List[str] = []
+    shipmentReference: Optional[str] = None
     shipmentReferenceType: Optional[str] = None
-    shipperAccountNumber: Optional[int] = None
+    shipperAccountNumber: Optional[str] = None
     dateRangeFrom: Optional[str] = None
     dateRangeTo: Optional[str] = None
     trackingView: Optional[str] = None
