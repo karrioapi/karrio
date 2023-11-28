17b6f7f80af5162dd6ce90378135829632b70f2a
Daniel K <dan@karrio.io>
2023-11-28 15:11:56 -0800

    (fix) dashboard sentry edge setup

--- apps/dashboard/sentry.edge.config.js
+++ apps/dashboard/sentry.edge.config.js
@@ -1,11 +1,9 @@
 // sentry.edge.config.js or sentry.edge.config.ts
 
 import * as Sentry from "@sentry/nextjs";
-import getConfig from 'next/config';
 
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-const { publicRuntimeConfig } = getConfig();
-const SENTRY_DSN = publicRuntimeConfig.SENTRY_DSN;
 
 Sentry.init({
     dsn: SENTRY_DSN,
