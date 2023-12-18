602cfa770564b51862b37d33b84a1e587514a4a2
Daniel K <dan@karrio.io>
2023-12-17 22:12:41 -0800

    (bump) up release version

--- modules/connectors/allied_express/setup.py
+++ modules/connectors/allied_express/setup.py
@@ -1,4 +1,3 @@
-
 """Warning: This setup.py is only there for git install until poetry support git subdirectory"""
 from setuptools import setup, find_namespace_packages
 
@@ -7,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="karrio.allied_express",
-    version="2023.12",
+    version="2023.12.1",
     description="Karrio - Allied Express Shipping Extension",
     long_description=long_description,
     long_description_content_type="text/markdown",
