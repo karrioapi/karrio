2d0bce345d96de154451b582dccaa2a1e49e6c3d
Daniel K <dan@karrio.io>
2024-02-09 21:06:09 -0800

    chore: Update migratsions and API schemas

--- modules/connectors/australiapost/setup.py
+++ modules/connectors/australiapost/setup.py
@@ -1,5 +1,5 @@
-
 """Warning: This setup.py is only there for git install until poetry support git subdirectory"""
+
 from setuptools import setup, find_namespace_packages
 
 with open("README.md", "r") as fh:
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="karrio.australiapost",
-    version="2024.1",
+    version="2024.2",
     description="Karrio - Australia Post Shipping Extension",
     long_description=long_description,
     long_description_content_type="text/markdown",
