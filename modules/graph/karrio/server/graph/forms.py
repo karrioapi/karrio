56c54deb7448bdb2d361c18d1c490921ed536d5d
Daniel K <dan@karrio.io>
2024-03-23 02:30:53 -0700

    feat: Conceal sensitive email sending error from bubbling to frontend

--- modules/graph/karrio/server/graph/forms.py
+++ modules/graph/karrio/server/graph/forms.py
@@ -1,13 +1,17 @@
-from django import forms
-from django.contrib.auth import forms as auth
-from django.core.exceptions import ValidationError
-from django.contrib.auth.tokens import default_token_generator
+import logging
+import django.forms as forms
+import django.contrib.auth.forms as auth
+import django.core.exceptions as exceptions
+import django.contrib.auth.tokens as tokens
+import django_email_verification.confirm as confirm
 
-from karrio.server.conf import settings
-from karrio.server.user.forms import SignUpForm
-from django_email_verification.confirm import _get_validated_field
+import karrio.server.conf as conf
+import karrio.server.user.forms as user_forms
 
-class UserRegistrationForm(SignUpForm):
+logger = logging.getLogger(__name__)
+
+
+class UserRegistrationForm(user_forms.SignUpForm):
     pass
 
 
@@ -25,25 +29,31 @@ class ConfirmPasswordResetForm(auth.SetPasswordForm):
 
     def clean_token(self):
         token = self.cleaned_data["token"]
-        valid_link = default_token_generator.check_token(self.user, token)
+        valid_link = tokens.default_token_generator.check_token(self.user, token)
 
         if not valid_link:
-            raise ValidationError("invalid or expired url token")
+            raise exceptions.ValidationError("invalid or expired url token")
 
 
 class ResetPasswordRequestForm(auth.PasswordResetForm):
     redirect_url = forms.URLField()
-    from_email =  _get_validated_field("EMAIL_FROM_ADDRESS")
+    from_email = confirm._get_validated_field("EMAIL_FROM_ADDRESS")
 
     def save(self, **kwargs):
-        super().save(
-            **{
-                **kwargs,
-                "extra_email_context": dict(
-                    app_name=settings.app_name,
-                    redirect_url=self.cleaned_data["redirect_url"],
-                ),
-                "email_template_name": "karrio/password_reset_email.html",
-                "from_email" : self.from_email,
-            }
-        )
+        try:
+            super().save(
+                **{
+                    **kwargs,
+                    "extra_email_context": dict(
+                        app_name=conf.settings.app_name,
+                        redirect_url=self.cleaned_data["redirect_url"],
+                    ),
+                    "email_template_name": "karrio/password_reset_email.html",
+                    "from_email": self.from_email,
+                }
+            )
+        except Exception as e:
+            logger.error(f"An error occurred while sending the email: {e}")
+            raise exceptions.ValidationError(
+                "An error occurred while sending the email"
+            )
