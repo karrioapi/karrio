076cc938c479d08aed1deb1ccbe097b482e54feb
Daniel K <danielk.developer@gmail.com>
2024-05-10 05:47:31 -0700

    release: patch 2024.2.16

--- modules/manager/setup.py
+++ modules/manager/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="karrio.server.manager",
-    version="2024.2.3",
+    version="2024.2.16",
     description="Multi-carrier shipping API Shipments manager module",
     long_description=long_description,
     long_description_content_type="text/markdown",
