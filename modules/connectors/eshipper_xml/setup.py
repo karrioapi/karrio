d438539339548170c221c073c3bd6a3366909a3e
Daniel K <danielk.developer@gmail.com>
2024-05-12 20:02:26 -0700

    feat: Rename legacy eshipper integration eshipper_xml

---
+++ modules/connectors/eshipper_xml/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="karrio.eshipper_xml",
+    version="2023.5",
+    description="Karrio - eShipper XML Shipping extension",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/karrioapi/karrio",
+    author="karrio",
+    author_email="hello@karrio.io",
+    license="Apache-2.0",
+    packages=find_namespace_packages(exclude=["tests.*", "tests"]),
+    install_requires=["karrio"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+)
