903a8230f7a911ac4469590af71bf9de7e8a116a
Daniel K <danielk.developer@gmail.com>
2024-06-01 20:53:18 -0700

    feat: Introduce cli command to generate request class tree for data mapping

--- modules/cli/commands/utils.py
+++ modules/cli/commands/utils.py
@@ -1,3 +1,4 @@
+import importlib
 import pydoc
 import karrio.lib as lib
 
@@ -29,3 +30,33 @@ def format_dimension(code, dim):
         f'| { f" x ".join([str(d) for d in dim.values() if isinstance(d, float)]) } '
         f'| { f" x ".join([k for k in dim.keys() if isinstance(dim[k], float)]) }'
     )
+
+
+def instantiate_tree(cls, indent=0):
+    tree = f"{cls.__name__}(\n"
+    indent += 1
+    for name, typ in cls.__annotations__.items():
+        if typ.__name__ == "Optional" and hasattr(typ, "__args__"):
+            typ = typ.__args__[0]
+        if typ.__name__ == "List" and hasattr(typ, "__args__"):
+            typ = typ.__args__[0]
+            tree += (
+                " " * indent * 4
+                + f"{name}=[\n"
+                + " " * indent * 5
+                + f"{instantiate_tree(typ, indent + 1)}\n"
+                + " " * indent * 4
+                + "],\n"
+            )
+        elif hasattr(typ, "__annotations__"):
+            tree += " " * indent * 4 + f"{name}={instantiate_tree(typ, indent)},\n"
+        else:
+            tree += " " * indent * 4 + f"{name}=None,\n"
+    tree += " " * (indent - 1) * 4 + ")"
+    return tree
+
+
+def instantiate_class_from_module(module_name: str, class_name: str):
+    module = importlib.import_module(module_name)
+    cls = getattr(module, class_name)
+    return instantiate_tree(cls)
