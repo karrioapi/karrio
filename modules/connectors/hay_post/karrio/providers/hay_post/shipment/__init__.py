a3aa82b37dcb81d8e150ba3157857fad2d7f03fc
david-kocharyan <david.k@ucraft.com>
2024-07-12 15:31:04 +0400

    remove cancel imports

--- modules/connectors/hay_post/karrio/providers/hay_post/shipment/__init__.py
+++ modules/connectors/hay_post/karrio/providers/hay_post/shipment/__init__.py
@@ -3,7 +3,3 @@ from karrio.providers.hay_post.shipment.create import (
     parse_shipment_response,
     shipment_request,
 )
-from karrio.providers.hay_post.shipment.cancel import (
-    parse_shipment_cancel_response,
-    shipment_cancel_request,
-)
