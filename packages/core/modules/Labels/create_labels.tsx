bf8ed8fba0c212ec3e0fec748d5a924b1a8469d1
Daniel K <danielk.developer@gmail.com>
2024-12-09 19:43:47 -0800

    feat: add new components for custom apps

--- packages/core/modules/Labels/create_labels.tsx
+++ packages/core/modules/Labels/create_labels.tsx
@@ -1793,6 +1793,7 @@ export default function Page(pageProps: any) {
                                     {/* CARRIER OPTIONS SECTION */}
                                     {Object.keys(carrierOptions).length > 0 && (
                                       <div className="card mb-4 px-3 mx-2">
+                                        {/* @ts-ignore */}
                                         <Disclosure>
                                           {({ open }) => (
                                             <div className="block">
