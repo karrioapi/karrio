5cbd30abdb44c09c9686b903c550db64f9919aa7
Daniel K <danielk.developer@gmail.com>
2025-04-25 09:59:52 -0700

    docs: prepare documentation foundation

---
+++ apps/web/src/app/docs/developing/carrier-integration/_meta.ts
@@ -0,0 +1,22 @@
+export default {
+  "index": {
+    title: 'Getting Started',
+    type: 'page'
+  },
+  "schema-generation": {
+    title: 'Schema Generation',
+    type: 'page'
+  },
+  "metadata": {
+    title: 'Metadata',
+    type: 'page'
+  },
+  "data-mapping": {
+    title: 'Data Mapping',
+    type: 'page'
+  },
+  "api-requests": {
+    title: 'API Requests',
+    type: 'page'
+  },
+}
