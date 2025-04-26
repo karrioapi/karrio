9abaeab3ff8e8ba5d0f42492ec695530ae6e4464
Daniel K <danielk.developer@gmail.com>
2025-04-26 01:36:24 -0700

    feat: introduce plugins loader for karrio SDK

---
+++ modules/sdk/karrio/validators/__init__.py
@@ -0,0 +1,6 @@
+"""
+Karrio validators package.
+
+This package provides address validation functionality through various service providers.
+"""
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
