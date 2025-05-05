ca76dd04051e7da772673da0b5cddbbfcfc54e11
Daniel K <danielk.developer@gmail.com>
2025-05-04 22:54:11 -0700

    docs(WIP): platform docs and @karrio/elements intro

---
+++ apps/web/src/app/docs/platform/guides/_meta.ts
@@ -0,0 +1,18 @@
+export default {
+  "karrio-admin": {
+    title: 'Karrio Admin',
+    type: 'page'
+  },
+  "shipments-management": {
+    title: 'Shipments Management',
+    type: 'page'
+  },
+  "trackers-management": {
+    title: 'Trackers Management',
+    type: 'page'
+  },
+  "document-generation": {
+    title: 'Document Generation',
+    type: 'page'
+  },
+}
