56349e1a5ba0e574bb8c924b29a99ebf829c79d9
Daniel K <danielk.developer@gmail.com>
2025-05-04 22:54:11 -0700

    docs(WIP): platform docs and @karrio/elements intro

---
+++ apps/web/src/app/docs/platform/elements/_meta.ts
@@ -0,0 +1,18 @@
+export default {
+  "karrio-provider": {
+    title: 'Karrio Provider',
+    type: 'page'
+  },
+  "carrier-connections": {
+    title: 'Carrier Connections',
+    type: 'page'
+  },
+  "shipping-form": {
+    title: 'Shipping Form',
+    type: 'page'
+  },
+  "tracking-form": {
+    title: 'Tracking Form',
+    type: 'page'
+  }
+}
