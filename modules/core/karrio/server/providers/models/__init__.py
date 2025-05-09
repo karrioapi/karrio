f5c0683db09fe1cc695c5256e58dd678568ba537
Daniel K <danielk.developer@gmail.com>
2025-05-08 23:38:21 -0700

    feat: remove deprecated carrier specific models

--- modules/core/karrio/server/providers/models/__init__.py
+++ modules/core/karrio/server/providers/models/__init__.py
@@ -26,22 +26,3 @@ from karrio.server.providers.models.carrier import (
 import karrio.server.providers.extension.models as extensions
 
 logger = logging.getLogger(__name__)
-
-
-# Register karrio settings defined above
-MODELS: typing.Dict[str, typing.Any] = {}
-
-
-def register_extensions():
-    """Register karrio-server models extensions after gateway is initialized"""
-    for _, name, _ in pkgutil.iter_modules(extensions.__path__):  # type: ignore
-        if hasattr(karrio, 'gateway') and name in karrio.gateway.providers:
-            try:
-                extension = __import__(f"{extensions.__name__}.{name}", fromlist=[name])
-                MODELS.update({name: register_model(extension.SETTINGS)})
-            except Exception as e:
-                logger.warning(f'Failed to register extension "{name}" Model')
-                logger.exception(e)
-
-# Register extensions after gateway initialization
-register_extensions()
