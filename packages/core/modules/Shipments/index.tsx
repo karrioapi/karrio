21066585fbca2024a535090d084c30b695a12614
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-08-26 15:26:03 +0530

    fix: floating dev tool hiding next button in mobile view

--- packages/core/modules/Shipments/index.tsx
+++ packages/core/modules/Shipments/index.tsx
@@ -474,14 +474,16 @@ export default function Page(pageProps: any) {
 
             {/* Sticky Footer */}
             <div className="sticky bottom-0 left-0 right-0 z-10 bg-white border-t border-gray-200 shadow-lg">
-              <ShipmentPagination
-                currentOffset={filter.offset as number || 0}
-                pageSize={20}
-                totalCount={shipments?.page_info?.count || 0}
-                hasNextPage={shipments?.page_info?.has_next_page || false}
-                onPageChange={(offset) => updateFilter({ offset })}
-                className="px-2 py-3"
-              />
+              <div className="mr-16 md:mr-0">
+                <ShipmentPagination
+                  currentOffset={filter.offset as number || 0}
+                  pageSize={20}
+                  totalCount={shipments?.page_info?.count || 0}
+                  hasNextPage={shipments?.page_info?.has_next_page || false}
+                  onPageChange={(offset) => updateFilter({ offset })}
+                  className="px-2 py-3"
+                />
+              </div>
             </div>
           </>
         )}
