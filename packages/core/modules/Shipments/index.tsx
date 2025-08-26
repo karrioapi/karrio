81456f1dc3c05b29d206c01c3d9527b0371bf741
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-08-26 14:59:25 +0530

    feat: added sticky footer to contain pagination

--- packages/core/modules/Shipments/index.tsx
+++ packages/core/modules/Shipments/index.tsx
@@ -472,13 +472,17 @@ export default function Page(pageProps: any) {
               </TableBody>
             </Table>
 
-            <ShipmentPagination
-              currentOffset={filter.offset as number || 0}
-              pageSize={20}
-              totalCount={shipments?.page_info?.count || 0}
-              hasNextPage={shipments?.page_info?.has_next_page || false}
-              onPageChange={(offset) => updateFilter({ offset })}
-            />
+            {/* Sticky Footer */}
+            <div className="sticky bottom-0 left-0 right-0 z-10 bg-white border-t border-gray-200 shadow-lg">
+              <ShipmentPagination
+                currentOffset={filter.offset as number || 0}
+                pageSize={20}
+                totalCount={shipments?.page_info?.count || 0}
+                hasNextPage={shipments?.page_info?.has_next_page || false}
+                onPageChange={(offset) => updateFilter({ offset })}
+                className="px-2 py-3"
+              />
+            </div>
           </>
         )}
 
