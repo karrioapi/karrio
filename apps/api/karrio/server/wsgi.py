1b4c9f16f64f456128801bfafe5c93b182954b8b
Daniel K <danielk.developer@gmail.com>
2025-10-16 01:06:28 +0200

    feat: consolidate temporary JTL tenant registration and custom JWT support

--- apps/api/karrio/server/wsgi.py
+++ apps/api/karrio/server/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karrio.server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karrio.server.settings.main')
 
 application = get_wsgi_application()
