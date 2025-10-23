537fa8e3633d226f26243009a1a6fe1f3ea5e186
Daniel K <danielk.developer@gmail.com>
2025-10-23 14:39:03 +0200

    fix: HUEY initialization race condition causing duplicated instances with Redis connection failure

--- apps/api/karrio/server/asgi.py
+++ apps/api/karrio/server/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karrio.server.settings.main')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karrio.server.settings')
 os.environ.setdefault('DJANGO_ALLOW_ASYNC_UNSAFE', 'true')
 
 application = get_asgi_application()
