82ec31ee93d46834f2d8f5bd06d16baf8e91da39
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-11-08 08:28:34 +0530

    feat(mydhl): implement error response parsing

--- modules/connectors/mydhl/karrio/providers/mydhl/error.py
+++ modules/connectors/mydhl/karrio/providers/mydhl/error.py
@@ -4,6 +4,7 @@ import typing
 import karrio.lib as lib
 import karrio.core.models as models
 import karrio.providers.mydhl.utils as provider_utils
+import karrio.schemas.mydhl.error_response as mydhl
 
 
 def parse_error_response(
@@ -11,15 +12,26 @@ def parse_error_response(
     settings: provider_utils.Settings,
     **kwargs,
 ) -> typing.List[models.Message]:
-    errors: list = []  # compute the carrier error object list
+    responses = response if isinstance(response, list) else [response]
+    errors = [
+        lib.to_object(mydhl.ErrorResponseType, error)
+        for error in responses
+        if any(error.get(key) for key in ["detail", "message", "title"])
+    ]
 
     return [
         models.Message(
             carrier_id=settings.carrier_id,
             carrier_name=settings.carrier_name,
-            code="",
-            message="",
-            details={**kwargs},
+            code=str(error.status) if error.status else None,
+            message=error.detail or error.message or error.title or "",
+            details=lib.to_dict(
+                {
+                    **kwargs,
+                    "instance": error.instance,
+                    "title": error.title,
+                }
+            ),
         )
         for error in errors
     ]
