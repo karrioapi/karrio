b7a1322571e3fd34d614b862b7d16feb0de09af4
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-12-10 01:44:55 +0530

    fix: resolve circular import preventing plugin registration

--- modules/connectors/gls_group/karrio/mappers/gls_group/__init__.py
+++ modules/connectors/gls_group/karrio/mappers/gls_group/__init__.py
@@ -2,6 +2,3 @@
 from karrio.mappers.gls_group.mapper import Mapper
 from karrio.mappers.gls_group.proxy import Proxy
 from karrio.mappers.gls_group.settings import Settings
-
-# Re-export METADATA for gateway registration
-from karrio.plugins.gls_group import METADATA
