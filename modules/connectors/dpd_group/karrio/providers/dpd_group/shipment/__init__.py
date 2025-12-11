ce3dfff9c7fe9d12c8e5e2d0c596e55c7253ba38
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-12-11 19:46:05 +0530

    refactor: remove unsupported DPD META-API features

--- modules/connectors/dpd_group/karrio/providers/dpd_group/shipment/__init__.py
+++ modules/connectors/dpd_group/karrio/providers/dpd_group/shipment/__init__.py
@@ -3,7 +3,3 @@ from karrio.providers.dpd_group.shipment.create import (
     parse_shipment_response,
     shipment_request,
 )
-from karrio.providers.dpd_group.shipment.cancel import (
-    parse_shipment_cancel_response,
-    shipment_cancel_request,
-)
