5cad204a2a1091123d5d656828f8902775575dfa
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-12-11 19:46:05 +0530

    refactor: remove unsupported DPD META-API features

--- modules/connectors/dpd_group/karrio/mappers/dpd_group/mapper.py
+++ modules/connectors/dpd_group/karrio/mappers/dpd_group/mapper.py
@@ -19,42 +19,11 @@ class Mapper(mapper.Mapper):
         self, payload: models.PickupRequest
     ) -> lib.Serializable:
         return provider.pickup_request(payload, self.settings)
-    
-    def create_pickup_update_request(
-        self, payload: models.PickupUpdateRequest
-    ) -> lib.Serializable:
-        return provider.pickup_update_request(payload, self.settings)
-    
-    def create_cancel_pickup_request(
-        self, payload: models.PickupCancelRequest
-    ) -> lib.Serializable:
-        return provider.pickup_cancel_request(payload, self.settings)
-    
-    def create_cancel_shipment_request(
-        self, payload: models.ShipmentCancelRequest
-    ) -> lib.Serializable[str]:
-        return provider.shipment_cancel_request(payload, self.settings)
-    
-    
-    def parse_cancel_pickup_response(
-        self, response: lib.Deserializable[str]
-    ) -> typing.Tuple[models.ConfirmationDetails, typing.List[models.Message]]:
-        return provider.parse_pickup_cancel_response(response, self.settings)
-    
-    def parse_cancel_shipment_response(
-        self, response: lib.Deserializable[str]
-    ) -> typing.Tuple[models.ConfirmationDetails, typing.List[models.Message]]:
-        return provider.parse_shipment_cancel_response(response, self.settings)
-    
+
     def parse_pickup_response(
         self, response: lib.Deserializable[str]
     ) -> typing.Tuple[models.PickupDetails, typing.List[models.Message]]:
         return provider.parse_pickup_response(response, self.settings)
-    
-    def parse_pickup_update_response(
-        self, response: lib.Deserializable[str]
-    ) -> typing.Tuple[models.PickupDetails, typing.List[models.Message]]:
-        return provider.parse_pickup_update_response(response, self.settings)
     
     def parse_shipment_response(
         self, response: lib.Deserializable[str]
--- modules/connectors/dpd_group/karrio/providers/dpd_group/pickup/__init__.py
+++ modules/connectors/dpd_group/karrio/providers/dpd_group/pickup/__init__.py
@@ -4,11 +4,3 @@ from karrio.providers.dpd_group.pickup.create import (
     parse_pickup_response,
     pickup_request,
 )
-from karrio.providers.dpd_group.pickup.update import (
-    parse_pickup_update_response,
-    pickup_update_request,
-)
-from karrio.providers.dpd_group.pickup.cancel import (
-    parse_pickup_cancel_response,
-    pickup_cancel_request,
-)
