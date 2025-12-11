46daa88d8eb0d40924432bb2e4d4502756130f14
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-12-11 16:11:27 +0530

    refactor: remove old DPD Group API v1.1 implementation to restart with new META-API

--- modules/connectors/dpd_group/karrio/providers/__init__.py
+++
@@ -1,2 +0,0 @@
-# Karrio providers namespace package
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
--- modules/connectors/dpd_group/tests/dpd_group/fixture.py
+++
@@ -1,8 +0,0 @@
-import karrio.sdk as karrio
-
-gateway = karrio.gateway["dpd_group"].create(
-    dict(
-        api_key="test_api_key_12345",
-        test_mode=True,
-    )
-)
