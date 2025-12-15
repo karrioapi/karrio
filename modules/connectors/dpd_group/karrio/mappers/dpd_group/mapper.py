39cb4e7dcaebc5caaff1910a884a86e767a5d4d6
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-12-15 15:21:53 +0530

    feat: add rating methods using universal provider

--- modules/connectors/dpd_group/karrio/mappers/dpd_group/mapper.py
+++ modules/connectors/dpd_group/karrio/mappers/dpd_group/mapper.py
@@ -6,10 +6,20 @@ import karrio.api.mapper as mapper
 import karrio.core.models as models
 import karrio.providers.dpd_group as provider
 import karrio.mappers.dpd_group.settings as provider_settings
+import karrio.universal.providers.rating as universal_provider
 
 
 class Mapper(mapper.Mapper):
     settings: provider_settings.Settings
+
+    def create_rate_request(self, payload: models.RateRequest) -> lib.Serializable:
+        return universal_provider.rate_request(payload, self.settings)
+
+    def parse_rate_response(
+        self, response: lib.Deserializable[str]
+    ) -> typing.Tuple[typing.List[models.RateDetails], typing.List[models.Message]]:
+        return universal_provider.parse_rate_response(response, self.settings)
+
     def create_shipment_request(
         self, payload: models.ShipmentRequest
     ) -> lib.Serializable:
