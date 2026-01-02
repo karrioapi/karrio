f3aa22cbd9a0c20aa6a2cbbca16edb462ef41884
Ansh Dev Nagar <anshdevnagar@gmail.com>
2026-01-03 04:18:43 +0530

    fix: correct test module imports in Hermes tests/__init__.py

--- modules/connectors/hermes/tests/__init__.py
+++ modules/connectors/hermes/tests/__init__.py
@@ -1,3 +1,3 @@
-
-from hermes.test_pickup import *
-from hermes.test_shipment import *
+from tests.hermes.test_pickup import *
+from tests.hermes.test_rate import *
+from tests.hermes.test_shipment import *
