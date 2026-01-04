be645524626f9b645e0e3e87cf742c58212251f7
Ansh Dev Nagar <anshdevnagar@gmail.com>
2026-01-04 22:02:43 +0530

    fix: correct schema types to match Asendia API documentation

--- modules/connectors/asendia/karrio/schemas/asendia/manifest_response.py
+++ modules/connectors/asendia/karrio/schemas/asendia/manifest_response.py
@@ -7,8 +7,8 @@ import typing
 class ManifestResponseType:
     id: typing.Optional[str] = None
     createdAt: typing.Optional[str] = None
-    errorMessage: typing.Any = None
-    errorParcelIds: typing.Optional[typing.List[typing.Any]] = None
+    errorMessage: typing.Optional[str] = None
+    errorParcelIds: typing.Optional[typing.List[str]] = None
     status: typing.Optional[str] = None
     manifestDocumentLocation: typing.Optional[str] = None
     parcelsLocation: typing.Optional[str] = None
