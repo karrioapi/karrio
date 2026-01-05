bbc5ea5de07e72c0f0a045ff25a09de55c1d4b96
Ansh Dev Nagar <anshdevnagar@gmail.com>
2026-01-06 02:42:21 +0530

    feat: configure plugin metadata with services and options

--- modules/connectors/asendia/karrio/plugins/asendia/__init__.py
+++ modules/connectors/asendia/karrio/plugins/asendia/__init__.py
@@ -7,23 +7,21 @@ import karrio.providers.asendia.units as units
 import karrio.providers.asendia.utils as utils
 
 
-# This METADATA object is used by Karrio to discover and register this plugin
-# when loaded through Python entrypoints or local plugin directories.
-# The entrypoint is defined in pyproject.toml under [project.entry-points."karrio.plugins"]
 METADATA = PluginMetadata(
     id="asendia",
     label="Asendia",
-    description="Asendia shipping integration for Karrio",
+    description="Asendia international e-commerce shipping services",
+    status="beta",
     # Integrations
     Mapper=Mapper,
     Proxy=Proxy,
     Settings=Settings,
     # Data Units
     is_hub=False,
-    # options=units.ShippingOption,
-    # services=units.ShippingService,
-    connection_configs=units.ConnectionConfig,
+    options=units.ShippingOption,
+    services=units.ShippingService,
+    connection_configs=utils.ConnectionConfig,
     # Extra info
-    website="",
-    documentation="",
+    website="https://www.asendia.com",
+    documentation="https://www.asendia-sync.com/swagger-ui/index.html",
 )
