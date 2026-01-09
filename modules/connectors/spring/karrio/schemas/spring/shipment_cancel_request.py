b1a3ee8b1cde247a7a319875babb6f5436c3246d
Ansh Dev Nagar <anshdevnagar@gmail.com>
2026-01-09 20:26:12 +0530

    feat: generate Spring carrier Python dataclasses from JSON schemas

---
+++ modules/connectors/spring/karrio/schemas/spring/shipment_cancel_request.py
@@ -0,0 +1,16 @@
+import attr
+import jstruct
+import typing
+
+
+@attr.s(auto_attribs=True)
+class ShipmentType:
+    TrackingNumber: typing.Optional[str] = None
+    ShipperReference: typing.Optional[str] = None
+
+
+@attr.s(auto_attribs=True)
+class ShipmentCancelRequestType:
+    Apikey: typing.Optional[str] = None
+    Command: typing.Optional[str] = None
+    Shipment: typing.Optional[ShipmentType] = jstruct.JStruct[ShipmentType]
