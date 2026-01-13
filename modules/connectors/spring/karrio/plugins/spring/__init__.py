7f9f8e557df25a36fe5a969d779dc56713d9d43c
Ansh Dev Nagar <anshdevnagar@gmail.com>
2026-01-13 23:41:08 +0530

    fix: configure Spring plugin metadata with standard pattern

--- modules/connectors/spring/karrio/plugins/spring/__init__.py
+++ modules/connectors/spring/karrio/plugins/spring/__init__.py
@@ -1,29 +1,19 @@
-from karrio.core.metadata import PluginMetadata
-
-from karrio.mappers.spring.mapper import Mapper
-from karrio.mappers.spring.proxy import Proxy
-from karrio.mappers.spring.settings import Settings
+import karrio.core.metadata as metadata
+import karrio.mappers.spring as mappers
 import karrio.providers.spring.units as units
-import karrio.providers.spring.utils as utils
 
 
-# This METADATA object is used by Karrio to discover and register this plugin
-# when loaded through Python entrypoints or local plugin directories.
-# The entrypoint is defined in pyproject.toml under [project.entry-points."karrio.plugins"]
-METADATA = PluginMetadata(
+METADATA = metadata.PluginMetadata(
+    status="beta",
     id="spring",
     label="Spring",
-    description="Spring shipping integration for Karrio",
     # Integrations
-    Mapper=Mapper,
-    Proxy=Proxy,
-    Settings=Settings,
+    Mapper=mappers.Mapper,
+    Proxy=mappers.Proxy,
+    Settings=mappers.Settings,
     # Data Units
     is_hub=False,
-    # options=units.ShippingOption,
-    # services=units.ShippingService,
+    options=units.ShippingOption,
+    services=units.ShippingService,
     connection_configs=units.ConnectionConfig,
-    # Extra info
-    website="",
-    documentation="",
 )
