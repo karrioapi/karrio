23adae1918a8cd51228e284545b510cd2576ac82
Ansh Dev Nagar <anshdevnagar@gmail.com>
2026-01-16 21:43:40 +0530

    feat: add rating capability using CSV-based services

--- modules/connectors/spring/karrio/mappers/spring/proxy.py
+++ modules/connectors/spring/karrio/mappers/spring/proxy.py
@@ -3,10 +3,14 @@
 import karrio.lib as lib
 import karrio.api.proxy as proxy
 import karrio.mappers.spring.settings as provider_settings
+import karrio.universal.mappers.rating_proxy as rating_proxy
 
 
-class Proxy(proxy.Proxy):
+class Proxy(rating_proxy.RatingMixinProxy, proxy.Proxy):
     settings: provider_settings.Settings
+
+    def get_rates(self, request: lib.Serializable) -> lib.Deserializable[str]:
+        return super().get_rates(request)
 
     def create_shipment(self, request: lib.Serializable) -> lib.Deserializable[str]:
         """Create shipments using Spring OrderShipment API.
