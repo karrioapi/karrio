d26a98f91340b125c948258b7a2120ec5f68d27b
Ansh Dev Nagar <anshdevnagar@gmail.com>
2026-01-25 21:41:03 +0530

    fix: add proper JSON schemas from OpenAPI and fix schema generation

--- modules/connectors/parcelone/karrio/schemas/parcelone/__init__.py
+++ modules/connectors/parcelone/karrio/schemas/parcelone/__init__.py
@@ -1,6 +1,61 @@
 """ParcelOne API schema types."""
 
-from karrio.schemas.parcelone.shipping_request import *
-from karrio.schemas.parcelone.shipping_response import *
-from karrio.schemas.parcelone.tracking_response import *
-from karrio.schemas.parcelone.error import *
+# Import shipping request types
+from karrio.schemas.parcelone.shipping_request import (
+    CepSpecialType,
+    FormatType,
+    MaxChargesType,
+    CustomDetailType,
+    IntDocDataType,
+    PackageDimensionsType,
+    PackageVolumeClassType,
+    ServiceType,
+    PackageType,
+    BankAccountType,
+    ShipmentAddressType,
+    ShipmentContactType,
+    ShipFromDataType,
+    ShipToDataType,
+    ShippingDataType,
+    ShippingRequestType,
+)
+
+# Import shipping response types with prefixes to avoid collision
+from karrio.schemas.parcelone.shipping_response import (
+    ErrorType as ShipmentErrorType,
+    WarningType as ShipmentWarningType,
+    ActionResultType as ShipmentActionResultType,
+    TotalChargesType,
+    FormatType as ResponseFormatType,
+    DocumentsResultType as ShipmentDocumentsResultType,
+    PackageResultType as ShipmentPackageResultType,
+    ResultsType as ShipmentResultType,
+    ShippingResponseType,
+)
+
+# Import tracking response types
+from karrio.schemas.parcelone.tracking_response import (
+    EventType as TrackingEventType,
+    ResultsType as TrackingResultType,
+    TrackingResponseType,
+)
+
+# Import error types
+from karrio.schemas.parcelone.error import (
+    ErrorType as ErrorDetailType,
+    ErrorResponseType,
+)
+
+# Backwards compatibility aliases for shipping request types
+ShippingDataRequestType = ShippingRequestType
+ShipmentType = ShippingDataType
+AddressType = ShipmentAddressType
+ContactType = ShipmentContactType
+ShipToType = ShipToDataType
+ShipFromType = ShipFromDataType
+MeasurementType = PackageVolumeClassType
+DimensionsType = PackageDimensionsType
+AmountType = MaxChargesType
+ShipmentServiceType = ServiceType
+ShipmentPackageType = PackageType
+CEPSpecialType = CepSpecialType
