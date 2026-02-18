d54dc4dcdb4e2b68ff932dc14998d042055d5ace
Daniel K <danielk.developer@gmail.com>
2026-02-18 10:23:46 -0800

    feat: add return shipment support for USPS International

--- modules/connectors/usps_international/karrio/providers/usps_international/__init__.py
+++ modules/connectors/usps_international/karrio/providers/usps_international/__init__.py
@@ -7,6 +7,9 @@ from karrio.providers.usps_international.shipment import (
     parse_shipment_response,
     shipment_cancel_request,
     shipment_request,
+
+    parse_return_shipment_response,
+    return_shipment_request,
 )
 from karrio.providers.usps_international.pickup import (
     parse_pickup_cancel_response,
