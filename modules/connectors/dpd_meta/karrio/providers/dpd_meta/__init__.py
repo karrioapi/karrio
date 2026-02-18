dd6620126370b873342dc7caff632acf62f49a1d
Daniel K <danielk.developer@gmail.com>
2026-02-18 10:23:36 -0800

    feat: add return shipment support for DPD Meta

--- modules/connectors/dpd_meta/karrio/providers/dpd_meta/__init__.py
+++ modules/connectors/dpd_meta/karrio/providers/dpd_meta/__init__.py
@@ -3,6 +3,9 @@ from karrio.providers.dpd_meta.utils import Settings
 from karrio.providers.dpd_meta.shipment import (
     parse_shipment_response,
     shipment_request,
+
+    parse_return_shipment_response,
+    return_shipment_request,
 )
 from karrio.providers.dpd_meta.pickup import (
     parse_pickup_response,
